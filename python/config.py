f3c97e8a71fa5d4918e16c5229f20e6df7fc61d1
claudio.kopper <claudio.kopper@16731396-06f5-0310-8873-f7f720988828>
2016-06-18 22:58:55 +0000

    fix environ call
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/ckopper/skymap_scanner@147579 16731396-06f5-0310-8873-f7f720988828

--- python/config.py
+++ python/config.py
@@ -1,7 +1,7 @@
 import os
 
 GCD_base_dirs = [
-os.path.join(os.environ("HOME"), "PoleBaseGCDs"), # why can't we reach anything from the followup nodes???
+os.path.join(os.environ["HOME"], "PoleBaseGCDs"), # why can't we reach anything from the followup nodes???
 "file:///data/exp/IceCube/2016/internal-system/PoleBaseGCDs",
 "http://icecube:skua@convey.icecube.wisc.edu/data/exp/IceCube/2016/internal-system/PoleBaseGCDs"]
 
