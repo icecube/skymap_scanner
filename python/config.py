d81388a4bac2701caa93e75237bd2276bddec6d8
claudio.kopper <claudio.kopper@16731396-06f5-0310-8873-f7f720988828>
2017-05-19 15:51:53 +0000

    new location for GCD diff baseline files
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/ckopper/skymap_scanner@155843 16731396-06f5-0310-8873-f7f720988828

--- python/config.py
+++ python/config.py
@@ -2,6 +2,8 @@ import os
 
 GCD_base_dirs = [
 os.path.join(os.environ["HOME"], "PoleBaseGCDs"), # why can't we reach anything from the followup nodes???
+"file:///data/user/followup/baseline_gcds",
+"http://icecube:skua@convey.icecube.wisc.edu/data/user/followup/baseline_gcds",
 "file:///data/exp/IceCube/2016/internal-system/PoleBaseGCDs",
 "http://icecube:skua@convey.icecube.wisc.edu/data/exp/IceCube/2016/internal-system/PoleBaseGCDs"]
 
