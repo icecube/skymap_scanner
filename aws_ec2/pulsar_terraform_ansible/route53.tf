8bce98ebcbbfb945660d420bd81d633956d3544c
Claudio Kopper <ckopper@icecube.aq>
2020-01-26 12:56:28 -0500

    AWS does not support dualstack network load balancers right now. Use ipv4 for now

--- aws_ec2/pulsar_terraform_ansible/route53.tf
+++ aws_ec2/pulsar_terraform_ansible/route53.tf
@@ -3,5 +3,5 @@ resource "aws_route53_record" "pulsar" {
   name = var.external_dns_name
   type = "CNAME"
   ttl = "30"
-  records = ["dualstack.${aws_elb.default.dns_name}"]
+  records = ["${aws_elb.default.dns_name}"]
 }
