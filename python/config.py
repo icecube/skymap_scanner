ce5a4b891d354035af9e10b69a87139ad3c0aa64
Erik Blaufuss <blaufuss@gmail.com>
2021-12-11 17:40:24 -0500

    remove slack key

--- python/config.py
+++ python/config.py
@@ -8,7 +8,7 @@ os.path.join(os.environ["HOME"], "PoleBaseGCDs"), # why can't we reach anything 
 "http://icecube:skua@convey.icecube.wisc.edu/data/exp/IceCube/2016/internal-system/PoleBaseGCDs",
 "file:///cvmfs/icecube.opensciencegrid.org/users/steinrob/GCD/PoleBaseGCDs/"]
 
-slack_api_key = "xoxb-48595976148-uwCZqeJAKYiiU9Y6nEww7CVW"
+slack_api_key = ""
 # slack_channel = "#test_messaging"
 slack_channel = "#gfu_live"
 # slack_channel = "#amon-alerts"
