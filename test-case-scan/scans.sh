f5ed2ddf14334c036a1f35c72efc3534524925e5
Ric Evans <emejqz@gmail.com>
2022-03-25 16:45:58 -0500

    add `tests.yml` (will fail)

--- test-case-scan/scans.sh
+++ test-case-scan/scans.sh
@@ -1,3 +1,5 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+echo $SCRIPT_DIR
+
 /cvmfs/icecube.opensciencegrid.org/users/followup/metaprojects/combo-realtime/trunk/build/env-shell.sh python $SCRIPT_DIR/perform_scan.py -r "ssh submitter" -n 30 ic191001a_sim -c $SCRIPT_DIR/output_skymap_scanner --event $SCRIPT_DIR/ic191001a_sim.i3 --gcd_dir $SCRIPT_DIR
