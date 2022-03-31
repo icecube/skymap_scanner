873c65f90cc98ce8e3c6e95928bfd95aec1f92fc
Ric Evans <emejqz@gmail.com>
2022-03-31 18:49:57 -0500

    pass in pulsar docker image name as arg to `make_pulsar_server_config.sh`

--- skymap_scanner/make_pulsar_server_config.sh
+++ skymap_scanner/make_pulsar_server_config.sh
@@ -2,12 +2,12 @@
 
 # From the README.md
 
-docker exec -ti pulsar_local bin/pulsar-admin tenants create icecube
-docker exec -ti pulsar_local bin/pulsar-admin namespaces create icecube/skymap
-docker exec -ti pulsar_local bin/pulsar-admin namespaces set-deduplication icecube/skymap --enable
-docker exec -ti pulsar_local bin/pulsar-admin namespaces create icecube/skymap_metadata
-docker exec -ti pulsar_local bin/pulsar-admin namespaces set-deduplication icecube/skymap_metadata --enable
-docker exec -ti pulsar_local bin/pulsar-admin namespaces set-retention icecube/skymap_metadata --size -1 --time -1
+docker exec -ti $1 bin/pulsar-admin tenants create icecube
+docker exec -ti $1 bin/pulsar-admin namespaces create icecube/skymap
+docker exec -ti $1 bin/pulsar-admin namespaces set-deduplication icecube/skymap --enable
+docker exec -ti $1 bin/pulsar-admin namespaces create icecube/skymap_metadata
+docker exec -ti $1 bin/pulsar-admin namespaces set-deduplication icecube/skymap_metadata --enable
+docker exec -ti $1 bin/pulsar-admin namespaces set-retention icecube/skymap_metadata --size -1 --time -1
 
-docker exec -ti pulsar_local bin/pulsar-admin topics create-partitioned-topic persistent://icecube/skymap/to_be_scanned --partitions 6
-docker exec -ti pulsar_local bin/pulsar-admin topics create-partitioned-topic persistent://icecube/skymap/scanned --partitions 6
+docker exec -ti $1 bin/pulsar-admin topics create-partitioned-topic persistent://icecube/skymap/to_be_scanned --partitions 6
+docker exec -ti $1 bin/pulsar-admin topics create-partitioned-topic persistent://icecube/skymap/scanned --partitions 6
