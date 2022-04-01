f646f00e196d9836ff95c02758ed0d699c2bb00f
Ric Evans <emejqz@gmail.com>
2022-03-31 19:05:24 -0500

    remove `-t` flag from pulsar config script

--- skymap_scanner/make_pulsar_server_config.sh
+++ skymap_scanner/make_pulsar_server_config.sh
@@ -2,12 +2,12 @@
 
 # From the README.md
 
-docker exec -ti $1 bin/pulsar-admin tenants create icecube
-docker exec -ti $1 bin/pulsar-admin namespaces create icecube/skymap
-docker exec -ti $1 bin/pulsar-admin namespaces set-deduplication icecube/skymap --enable
-docker exec -ti $1 bin/pulsar-admin namespaces create icecube/skymap_metadata
-docker exec -ti $1 bin/pulsar-admin namespaces set-deduplication icecube/skymap_metadata --enable
-docker exec -ti $1 bin/pulsar-admin namespaces set-retention icecube/skymap_metadata --size -1 --time -1
+docker exec -i $1 bin/pulsar-admin tenants create icecube
+docker exec -i $1 bin/pulsar-admin namespaces create icecube/skymap
+docker exec -i $1 bin/pulsar-admin namespaces set-deduplication icecube/skymap --enable
+docker exec -i $1 bin/pulsar-admin namespaces create icecube/skymap_metadata
+docker exec -i $1 bin/pulsar-admin namespaces set-deduplication icecube/skymap_metadata --enable
+docker exec -i $1 bin/pulsar-admin namespaces set-retention icecube/skymap_metadata --size -1 --time -1
 
-docker exec -ti $1 bin/pulsar-admin topics create-partitioned-topic persistent://icecube/skymap/to_be_scanned --partitions 6
-docker exec -ti $1 bin/pulsar-admin topics create-partitioned-topic persistent://icecube/skymap/scanned --partitions 6
+docker exec -i $1 bin/pulsar-admin topics create-partitioned-topic persistent://icecube/skymap/to_be_scanned --partitions 6
+docker exec -i $1 bin/pulsar-admin topics create-partitioned-topic persistent://icecube/skymap/scanned --partitions 6
