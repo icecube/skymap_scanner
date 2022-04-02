b6303f26856c5544a41e9c347518dce9df9233f8
Ric Evans <emejqz@gmail.com>
2022-04-01 19:18:41 -0500

    (temp comment out producer & worker launch)

--- skymap_scanner/tests/docker_scripts/launch.sh
+++ skymap_scanner/tests/docker_scripts/launch.sh
@@ -10,7 +10,7 @@ set -x
 # docker run --rm -i $1 producer $2 --broker pulsar://localhost:6650 --nside 1 -n test_event_01
 
 # Then you can then start some workers to scan the jobs in the queue:
-docker run --rm -i $1 worker --broker pulsar://localhost:6650
+# docker run --rm -i $1 worker --broker pulsar://localhost:6650
 
 # Finally, since there are 7 jobs per pixel (different reconstruction
 # seeds in absolute position in the detector), they need to be collected
