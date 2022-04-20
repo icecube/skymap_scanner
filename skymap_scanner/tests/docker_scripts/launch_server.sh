2069ec64bfb2224d8b4c77fd7fdb38a02902543c
Ric Evans <emejqz@gmail.com>
2022-04-20 14:20:40 -0500

    use the `event_HESE_2017-11-28.json` already in `tests/` (not from url)

--- skymap_scanner/tests/docker_scripts/launch_server.sh
+++ skymap_scanner/tests/docker_scripts/launch_server.sh
@@ -4,4 +4,4 @@
 
 set -x
 
-docker run --rm -i $1 server $SKYSCAN_EVENT_URL --broker pulsar://localhost:6650 --nside 1 -n $SKYSCAN_TEST_EVENT
+docker run --rm -i $1 server $SKYSCAN_EVENT --broker pulsar://localhost:6650 --nside 1 -n $SKYSCAN_TEST_EVENT
