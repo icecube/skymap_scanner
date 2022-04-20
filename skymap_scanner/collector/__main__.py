7c328a43126c443e0df8ad6a350a49c689372ea7
Ric Evans <emejqz@gmail.com>
2022-04-19 19:39:48 -0500

    remove `collector` and `saver` sub-packages

--- skymap_scanner/collector/__main__.py
+++
@@ -1,6 +0,0 @@
-"""Entry-point to start up collector service."""
-
-from . import collector
-
-if __name__ == "__main__":
-    collector.main()
--- skymap_scanner/saver/__main__.py
+++
@@ -1,6 +0,0 @@
-"""Entry-point to start up saver service."""
-
-from . import saver
-
-if __name__ == "__main__":
-    saver.main()
