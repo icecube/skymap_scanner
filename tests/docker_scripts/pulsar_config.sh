8ec79425b698e2f81b00b7a1016d51c99bd498a9
Ric Evans <emejqz@gmail.com>
2022-05-04 18:07:11 -0500

    remove pre-allocating namespaces/topics

--- tests/docker_scripts/pulsar_config.sh
+++ tests/docker_scripts/pulsar_config.sh
@@ -10,12 +10,12 @@ wget https://github.com/jwilder/dockerize/releases/download/$DOCKERIZE_VERSION/d
 dockerize -wait tcp://localhost:8080 -timeout 1m
 dockerize -wait tcp://localhost:6650 -timeout 1m
 
-docker exec -i $1 bin/pulsar-admin tenants create icecube
-docker exec -i $1 bin/pulsar-admin namespaces create icecube/skymap
-docker exec -i $1 bin/pulsar-admin namespaces set-deduplication icecube/skymap --enable
-docker exec -i $1 bin/pulsar-admin namespaces create icecube/skymap_metadata
-docker exec -i $1 bin/pulsar-admin namespaces set-deduplication icecube/skymap_metadata --enable
-docker exec -i $1 bin/pulsar-admin namespaces set-retention icecube/skymap_metadata --size -1 --time -1
+# docker exec -i $1 bin/pulsar-admin tenants create icecube
+# docker exec -i $1 bin/pulsar-admin namespaces create icecube/skymap
+# docker exec -i $1 bin/pulsar-admin namespaces set-deduplication icecube/skymap --enable
+# docker exec -i $1 bin/pulsar-admin namespaces create icecube/skymap_metadata
+# docker exec -i $1 bin/pulsar-admin namespaces set-deduplication icecube/skymap_metadata --enable
+# docker exec -i $1 bin/pulsar-admin namespaces set-retention icecube/skymap_metadata --size -1 --time -1
 
-docker exec -i $1 bin/pulsar-admin topics create-partitioned-topic persistent://icecube/skymap/to_be_scanned --partitions 6
-docker exec -i $1 bin/pulsar-admin topics create-partitioned-topic persistent://icecube/skymap/scanned --partitions 6
+# docker exec -i $1 bin/pulsar-admin topics create-partitioned-topic persistent://icecube/skymap/to_be_scanned --partitions 6
+# docker exec -i $1 bin/pulsar-admin topics create-partitioned-topic persistent://icecube/skymap/scanned --partitions 6
