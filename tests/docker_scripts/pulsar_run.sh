d2f28c9e58a24015f5d624b334bdd2e6855aafb1
Ric Evans <emejqz@gmail.com>
2022-05-04 17:53:04 -0500

    format `pulsar_run.sh`

--- tests/docker_scripts/pulsar_run.sh
+++ tests/docker_scripts/pulsar_run.sh
@@ -2,4 +2,8 @@
 
 set -x
 
-docker run -i -d --rm -p 6650:6650 -p 8080:8080 --name $1 apachepulsar/pulsar:2.6.0 /bin/bash -c "sed -i s/brokerDeleteInactiveTopicsEnabled=.*/brokerDeleteInactiveTopicsEnabled=false/ /pulsar/conf/standalone.conf && bin/pulsar standalone"
+docker run -i -d --rm \
+    -p 6650:6650 \
+    -p 8080:8080 \
+    --name $1 apachepulsar/pulsar:2.6.0 /bin/bash \
+    -c "sed -i s/brokerDeleteInactiveTopicsEnabled=.*/brokerDeleteInactiveTopicsEnabled=false/ /pulsar/conf/standalone.conf && bin/pulsar standalone"
