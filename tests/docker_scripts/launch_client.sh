d0467831058abe4a8dedcd15f5252a427affd087
Ric Evans <emejqz@gmail.com>
2022-05-04 18:10:02 -0500

    try just `--broker localhost` (no port)

--- tests/docker_scripts/launch_client.sh
+++ tests/docker_scripts/launch_client.sh
@@ -6,5 +6,5 @@ set -x
 
 docker run --rm -i $1 skymap_scanner.client \
     --event-id $SKYSCAN_EVENT \
-    --broker localhost:6650 \
+    --broker localhost \
     --log DEBUG
--- tests/docker_scripts/launch_server.sh
+++ tests/docker_scripts/launch_server.sh
@@ -6,5 +6,5 @@ set -x
 
 docker run --rm -i $1 skymap_scanner.server \
     --event-id $SKYSCAN_EVENT \
-    --broker localhost:6650 \
+    --broker localhost \
     --log DEBUG
