b8f1478cad429b11a05f702aba0823c0563f55ef
Ric Evans <emejqz@gmail.com>
2022-05-05 16:32:24 -0500

    fix bash error

--- tests/docker_scripts/launch_server.sh
+++ tests/docker_scripts/launch_server.sh
@@ -5,7 +5,7 @@
 set -x
 
 docker run --network="host" --rm -i $1 skymap_scanner.server \
-    --cache-dir SKYSCAN_CACHE_DIR \
+    --cache-dir $SKYSCAN_CACHE_DIR \
     --event-id $SKYSCAN_EVENT \
     --broker $PULSAR_ADDRESS \
     --log DEBUG
