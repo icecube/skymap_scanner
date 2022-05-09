1b74b4bea76799e1e46b107019b804214e8ca299
Ric Evans <emejqz@gmail.com>
2022-05-09 16:55:13 -0500

    docker run: add bind mount for cache dir

--- tests/docker_scripts/launch_server.sh
+++ tests/docker_scripts/launch_server.sh
@@ -5,9 +5,11 @@
 set -x
 
 mkdir $SKYSCAN_CACHE_DIR
-docker run --network="host" --rm -i $1 skymap_scanner.server \
+docker run --network="host" --rm \
+    --mount type=bind,source=$SKYSCAN_CACHE_DIR,target=/local/cache-dir \
+    -i $1 skymap_scanner.server \
     --event-pkl $SKYSCAN_EVENT_PKL \
-    --cache-dir $SKYSCAN_CACHE_DIR \
+    --cache-dir /local/cache-dir \
     --broker $PULSAR_ADDRESS \
     --auth-token $PULSAR_AUTH \
     --log DEBUG
