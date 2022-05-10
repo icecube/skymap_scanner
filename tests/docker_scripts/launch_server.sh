166aa84df278b4c66be85298af6a405149a9d6d8
Ric Evans <emejqz@gmail.com>
2022-05-10 16:26:19 -0500

    docker run/server: try just mounting `$SKYSCAN_GCD_DIR`

--- tests/docker_scripts/launch_server.sh
+++ tests/docker_scripts/launch_server.sh
@@ -7,10 +7,10 @@ set -x
 mkdir "$(pwd)"/$SKYSCAN_CACHE_DIR
 docker run --network="host" --rm -i \
     --mount type=bind,source="$(pwd)"/$SKYSCAN_CACHE_DIR,target=/local/$SKYSCAN_CACHE_DIR \
-    --mount type=bind,source=/cvmfs/,target=/cvmfs/ \
+    --mount type=bind,source=$SKYSCAN_GCD_DIR,target=/local/gcd-dir \
     $1 skymap_scanner.server \
     --event-pkl $SKYSCAN_EVENT_PKL \
     --cache-dir /local/$SKYSCAN_CACHE_DIR \
-    --gcd-dir $SKYSCAN_GCD_DIR \
+    --gcd-dir /local/gcd-dir \
     --broker $PULSAR_ADDRESS \
     --log DEBUG
