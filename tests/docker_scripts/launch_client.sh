782a65efa4cf16bd38c18565d7b2765774d25e3f
Ric Evans <emejqz@gmail.com>
2022-05-11 17:55:20 -0500

    client docker run: bind-mount cvmfs

--- tests/docker_scripts/launch_client.sh
+++ tests/docker_scripts/launch_client.sh
@@ -5,6 +5,7 @@
 set -x
 
 docker run --network="host" --rm -i \
+    --mount type=bind,source=$SKYSCAN_GCD_DIR,target=/local/gcd-dir \
     --env PY_COLORS=1 \
     $1 skymap_scanner.client \
     --event-name $SKYSCAN_EVENT_PKL \
