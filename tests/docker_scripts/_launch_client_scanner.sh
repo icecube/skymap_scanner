4d6ab7a52e86112de46dc175a495302a75897c02
Ric Evans <emejqz@gmail.com>
2022-05-17 15:29:29 -0500

    CI: mount directory for `test-run-client-scanner` (still needs files)

--- tests/docker_scripts/_launch_client_scanner.sh
+++ tests/docker_scripts/_launch_client_scanner.sh
@@ -4,8 +4,9 @@ set -x
 
 docker run --network="host" --rm -i \
     --mount type=bind,source=$SKYSCAN_GCD_DIR,target=/local/gcd-dir \
+    --mount type=bind,source=$SKYSCAN_CLIENT_SCANNER_FILES_DIR,target=/local/in-out-files \
     --env PY_COLORS=1 \
     $1 skymap_scanner.client.scan_pixel \
-    --in-file $SKYSCAN_CLIENT_SCANNER_IN_FILE \
-    --out-file $SKYSCAN_CLIENT_SCANNER_OUT_FILE \
+    --in-file /local/in-out-files/$SKYSCAN_CLIENT_SCANNER_IN_FILENAME \
+    --out-file /local/in-out-files/$SKYSCAN_CLIENT_SCANNER_OUT_FILENAME \
     --log DEBUG
