497d29fd561c344ff5ca7814bbc97824b783b9e3
Massimiliano Lincetto <m.lincetto@gmail.com>
2022-05-31 18:01:51 +0200

    dummy processor getting dangerous

--- resources/scripts/alert_processor.py
+++ resources/scripts/alert_processor.py
@@ -1,12 +1,27 @@
 import pickle
 import argparse
+import logging
+from pathlib import Path
+
+from daemon_lib import RealtimeEvent
 
 if __name__ == '__main__':
+    log = logging.getLogger(__name__)
+
     parser = argparse.ArgumentParser(description='Millipede Scanner')
     parser.add_argument('-e', '--event', help='Event file to process')
     args = parser.parse_args()
 
-    with open(args.event, 'rb') as event_file:
-        event = pickle.load(event_file)
+    event_filepath = Path(args.event)
 
-    print(event)
+    with open(event_filepath, 'rb') as event_file:
+        event_dict = pickle.load(event_file)
+
+    event_object = RealtimeEvent(event_dict)
+
+    log.info(
+        f'Read {args.event} corresponding to f{event_object.get_unique_id()}')
+
+    log.info('Nothing to be done (at the moment), cleaning up the event file')
+
+    event_filepath.unlink()
