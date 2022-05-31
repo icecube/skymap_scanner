4cc97afdbf542ed37057d6bfaad4685d1141bc76
Massimiliano Lincetto <m.lincetto@gmail.com>
2022-05-31 14:34:55 +0200

    tentative implementation of a cache manager class

--- resources/scripts/alert_daemon.py
+++ resources/scripts/alert_daemon.py
@@ -10,30 +10,42 @@ from slack_tools import MessageHelper as msg
 
 from daemon_conf import shifters_slackid
 
-
-def get_cache_path():
-    # logic copied from realtime_tools/python/config.py
-    tmp_path = os.path.expandvars('/scratch/$USER')
-    if not os.path.isdir(tmp_path):
-        # use system temp directory when scratch is not available
-        tmp_path = tempfile.gettempdir()
-    return tmp_path
+import tempfile
 
 
-def make_cache_dir(cache_name='skymap_scanner_cache'):
-    cache_dir = os.path.join(
-        get_cache_path(), cache_name)
-    if not os.path.isdir(cache_dir):
-        os.makedirs(cache_dir)
-        # maybe shlould go in a try / except OSError block
-    return cache_dir
+class CacheManager():
+    def __init__(self, name='skymap_scanner_cache'):
+        self.path = self.get_cache_path()
+        self.cache_dir = self.make_cache_dir(cache_name=name)
+
+    def get_cache_path(self):
+        # logic borrowed from realtime_tools/python/config.py
+        tmp_path = os.path.expandvars('/scratch/$USER')
+        if not os.path.isdir(tmp_path):
+            # use system temp directory when scratch is not available
+            tmp_path = tempfile.gettempdir()
+        return tmp_path
+
+    def make_cache_dir(self, cache_name='skymap_scanner_cache'):
+        cache_dir = os.path.join(
+            self.path, cache_name)
+        if not os.path.isdir(cache_dir):
+            # TODO: maybe should go in a try / except OSError block
+            os.makedirs(cache_dir)
+        return cache_dir
+
+    @property
+    def dir(self):
+        return self.cache_dir
 
 
 def handle_event(varname, topics, event):
 
     uid = 'evt_' + event["time"]  # no need to hash here (?)
 
-    event_cache_dir = make_cache_dir()  # not optimal
+    cache_manager = CacheManager()
+
+    event_cache_dir = cache_manager.dir  # not optimal
 
     event_filename = uid + '.pkl'
     event_filepath = os.path.join(event_cache_dir, event_filename)
