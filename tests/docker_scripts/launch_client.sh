666d9ea0b59354eea98f61bc38e72c677ffb0044
Ric Evans <emejqz@gmail.com>
2022-06-01 18:15:38 -0500

    add `$SKYSCAN_DEBUG_DIR`

--- tests/docker_scripts/launch_client.sh
+++ tests/docker_scripts/launch_client.sh
@@ -2,10 +2,13 @@
 
 set -x
 
+mkdir "$(pwd)"/$SKYSCAN_DEBUG_DIR
 docker run --network="host" --rm -i \
     --mount type=bind,source=$SKYSCAN_GCD_DIR,target=/local/gcd-dir \
+    --mount type=bind,source="$(pwd)"/$SKYSCAN_DEBUG_DIR,target=/local/$SKYSCAN_DEBUG_DIR \
     --env PY_COLORS=1 \
     $1 skymap_scanner.client \
     --event-name $SKYSCAN_EVENT_PKL \
     --broker $PULSAR_ADDRESS \
-    --log DEBUG
+    --log DEBUG \
+    --debug-directory /local/$SKYSCAN_DEBUG_DIR
