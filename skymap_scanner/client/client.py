a9a39eb21a01a0803b9ad3da349b3d14c9b3bbc0
Ric Evans <emejqz@gmail.com>
2022-06-07 12:21:08 -0500

    client: name change fix

--- skymap_scanner/client/client.py
+++ skymap_scanner/client/client.py
@@ -97,7 +97,7 @@ async def scan_pixel_distributed(
 
             # call & check outputs
             cmd = (
-                f"python -m skymap_scanner.client.scan_pixel "
+                f"python -m skymap_scanner.client.scan_pixel_pkl "
                 f"--in-file {IN} "
                 f"--out-file {OUT} "
                 f"--log {logging.getLevelName(LOGGER.getEffectiveLevel())}"
