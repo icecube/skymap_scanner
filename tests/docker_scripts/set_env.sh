b211ce5b040a171418203031e6457cbb92253e10
Ric Evans <emejqz@gmail.com>
2022-06-10 12:23:49 -0500

    CI: fix relative paths

--- tests/docker_scripts/set_env.sh
+++ tests/docker_scripts/set_env.sh
@@ -5,7 +5,6 @@
 export PULSAR_CONTAINER=pulsar_local
 export SKYSCAN_CONTAINER=skyscan-cloud
 export SKYSCAN_CACHE_DIR=cache-dir
-export REALTIME_EVENTS_DIR=./tests/data/skyscan_data/realtime_events/
 export PULSAR_ADDRESS=localhost:6650
 export PULSAR_AUTH=""
 export SKYSCAN_DEBUG_DIR=debug-pkl-dir
