334dbecf6bc0ca8a1c17d51e2a4bff520f868eb7
Massimiliano Lincetto <m.lincetto@gmail.com>
2022-06-10 16:37:05 +0200

    optional extraction

--- alert_listener/realtime_event.py
+++ alert_listener/realtime_event.py
@@ -7,60 +7,62 @@ import json
 from icecube import full_event_followup  # , frame_object_diff
 
 
-class RealtimeEvent():
+class RealtimeEvent:
     """
     The implementation of this class assumes that the event dictionary is structured according to the new (2018) data format, with the data frames accessible under key value/data/frames. The pre-2018 format has frames in value/data and is supported in IceTray (see `full_event_followup/python/frame_packet_to_i3live_json.py`). The original `skymap_scanner` code assumed the new format, so the old one is not meant to be supported here (unless required).
     """
 
-    def __init__(self, event) -> None:
+    def __init__(self, event, extract_frames=False) -> None:
         self.event = event
-        #
-        self.frame_packet = self.extract_frame_packet()
 
-        self.frame_packet_control = full_event_followup.i3live_json_to_frame_packet(
-            json.dumps(self.event), pnf_framing=True)
+        if extract_frames:
+            self.frame_packet = self.extract_frame_packet()
 
-        # TODO: delete self.event['value']['data']['frames']
+            self.frame_packet_control = full_event_followup.i3live_json_to_frame_packet(
+                json.dumps(self.event), pnf_framing=True
+            )
+
+            del self.event["value"]["data"]["frames"]
 
         pass
 
     def get_frame_list(self) -> list:
-        return self.event['value']['data']['frames']
+        return self.event["value"]["data"]["frames"]
 
     def extract_frame_packet(self):
         frame_list = self.get_frame_list()
         return FramePacket(frame_list)
 
     def get_message_time(self):
-        return self.event['time']
+        return self.event["time"]
 
     def get_uid(self):
-        return self.event['value']['data']['unique_id']
+        return self.event["value"]["data"]["unique_id"]
 
     def get_run(self):
-        return self.event['value']['data']['run_id']
+        return self.event["value"]["data"]["run_id"]
 
     def get_event_number(self):
-        return self.event['value']['data']['event_id']
+        return self.event["value"]["data"]["event_id"]
 
     def get_stem(self):
-        '''
-            filename stem based on evt and run number
-            in origin this was based on a hash of event['time']
-            to be verified if this new approach is equally robust
-        '''
+        """
+        filename stem based on evt and run number
+        in origin this was based on a hash of event['time']
+        to be verified if this new approach is equally robust
+        """
         uid = self.get_uid()
         # dashes are preferred to dots in directory names
-        stem = uid.replace('.', '-')
+        stem = uid.replace(".", "-")
 
         return stem
 
     def get_alert_streams(self):
         # previous code used this for no apparent good reason:
         # [str(x) for x in event["value"]["streams"]]
-        return self.event['value']['streams']
+        return self.event["value"]["streams"]
 
-    '''
+    """
     def stem_from_message_time(self):
         # provides uid based on timestamp
         # tentatively superseded
@@ -71,10 +73,10 @@ class RealtimeEvent():
         buf = buf.replace(':', '')
         buf = buf.replace('.', sep)
         return buf
-    '''
+    """
 
 
-class FramePacket():
+class FramePacket:
     def __init__(self, frame_list) -> None:
 
         self.logger = logging.getLogger(__name__)
@@ -114,10 +116,10 @@ class FramePacket():
 
         try:
             frame_object = pickle.loads(decompressed_data, encoding="bytes")
-            self.logger.info('Frame unpickled with byte encoding')
+            self.logger.info("Frame unpickled with byte encoding")
         except TypeError:  # this is the python 2 version
             frame_object = pickle.loads(decompressed_data)
-            self.logger.warning('Frame unpickled with default encoding')
+            self.logger.warning("Frame unpickled with default encoding")
 
         del decompressed_data  # is it really needed?
 
@@ -125,6 +127,7 @@ class FramePacket():
 
         if frame_object.Stop.id != frame_id:
             raise ValueError(
-                f"Frame ID mismatch between content - frame {frame_object.Stop.id} - and dictionary has {frame_id}")
+                f"Frame ID mismatch between content - frame {frame_object.Stop.id} - and dictionary has {frame_id}"
+            )
 
         return frame_object
