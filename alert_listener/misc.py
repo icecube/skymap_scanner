0618e53381dc1eff8080494fdf55474398fd6319
Massimiliano Lincetto <m.lincetto@gmail.com>
2022-06-13 09:34:28 -0500

    drop orphan code

--- alert_listener/misc.py
+++ alert_listener/misc.py
@@ -1,18 +1,12 @@
-# Could be useful to have a logger that can optionally post to slack?
-# UNTESTED
-
-from logging import Logger
-
-class FlexiLogger(Logger):
-    # untested; not sure this works because of how super() is resolved
-    log_func = { 'info':super().info, 'debug':super().debug, 'warning':super().warning, 'error':super().error }
-
-    def __init__(self, SlackInterface):
-        self.slack = SlackInterface
-        self.log = logging.getLogger(__name__)
-
-    def log(level='info', slack=False, message=None):
-        if slack:
-            self.slack.post(message)
-        self.log_func[level](message)
-
+"""
+    def stem_from_message_time(self):
+        # provides uid based on timestamp
+        # tentatively superseded
+        sep = '-'
+        buf = self.event['time']
+        buf = buf.replace('-', '')
+        buf = buf.replace(' ', sep)
+        buf = buf.replace(':', '')
+        buf = buf.replace('.', sep)
+        return buf
+"""
