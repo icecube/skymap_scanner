e9e3766af09c6714be09bc16c7941aa9a006e612
Massimiliano Lincetto <m.lincetto@gmail.com>
2022-06-14 11:21:04 -0500

    event directory allocation and use of pathlib

--- alert_listener/cache_manager.py
+++ alert_listener/cache_manager.py
@@ -1,13 +1,15 @@
 import os
 import tempfile
+from pathlib import Path
 
 
 class CacheManager:
     def __init__(self, name="skymap_scanner_cache"):
-        self.path = self.get_cache_path()
+        self.loc = self.get_cache_location()
         self.cache_dir = self.make_cache_dir(cache_name=name)
+        self.path = Path(self.cache_dir)
 
-    def get_cache_path(self):
+    def get_cache_location(self):
         # logic borrowed from realtime_tools/python/config.py
         tmp_path = os.path.expandvars("/scratch/$USER")
         if not os.path.isdir(tmp_path):
@@ -16,11 +18,17 @@ class CacheManager:
         return tmp_path
 
     def make_cache_dir(self, cache_name):
-        cache_dir = os.path.join(self.path, cache_name)
+        cache_dir = os.path.join(self.loc, cache_name)
         if not os.path.isdir(cache_dir):
             # TODO: maybe should go in a try / except OSError block
             os.makedirs(cache_dir)
         return cache_dir
+
+    def allocate_dir(self, dir_name):
+        dir_path = self.path / Path(dir_name)
+        if not dir_path.is_dir():
+            dir_path.mkdir()
+        return dir_path
 
     @property
     def dir(self):
