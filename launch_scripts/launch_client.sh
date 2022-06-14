1d38c3fd24fd5531676f794232d3bbad1a286885
Ric Evans <emejqz@gmail.com>
2022-06-14 16:55:16 -0500

    `launch_client.sh`: make args user-friendly (auto-mounting)

--- launch_scripts/launch_client.sh
+++ launch_scripts/launch_client.sh
@@ -2,14 +2,41 @@
 
 set -x
 
-mkdir "$(pwd)"/$SKYSCAN_DEBUG_DIR
+# Get & transform arguments that are files/dirs for docker-mounting
+# yes, this is simpler than a bash-native solution
+export ARGS="$*" # all of the arguments stuck together into a single string
+DOCKER_PY_ARGS=$(python3 -c '
+import os
+py_args = os.getenv("ARGS")
+
+def extract_opt(py_args, opt):
+    before, after = py_args.split(f" {opt} ", 1)
+    val, after = after.split(" ", 1)
+    return f"{before} {after}", val
+
+py_args, debug_dir = extract_opt(py_args, "--debug-directory")
+py_args, gcd = extract_opt(py_args, "--gcd-dir")
+
+py_args = (
+    f"{py_args} "
+    f"--debug-directory /local/{os.path.basename(debug_dir)} "
+    f"--gcd-dir /local/gcd-dir"
+)
+
+dockermount_args = (
+    f"--mount type=bind,source={debug_dir},target=/local/{os.path.basename(debug_dir)} "
+    f"--mount type=bind,source={gcd},target=/local/gcd-dir,readonly "
+)
+
+print(f"{dockermount_args}#{py_args}")
+')
+DOCKERMOUNT_ARGS="$(echo $DOCKER_PY_ARGS | awk -F "#" '{print $1}')"
+PY_ARGS="$(echo $DOCKER_PY_ARGS | awk -F "#" '{print $2}')"
+
+# Run
 docker run --network="host" --rm -i \
     --shm-size=6gb \
-    --mount type=bind,source=$SKYSCAN_GCD_DIR,target=/local/gcd-dir,readonly \
-    --mount type=bind,source="$(pwd)"/$SKYSCAN_DEBUG_DIR,target=/local/$SKYSCAN_DEBUG_DIR \
+    $DOCKERMOUNT_ARGS \
     --env PY_COLORS=1 \
-    $1 skymap_scanner.client \
-    --event-name $2 \
-    --broker $PULSAR_ADDRESS \
-    --log DEBUG \
-    --debug-directory /local/$SKYSCAN_DEBUG_DIR
+    $SKYSCAN_CONTAINER skymap_scanner.client \
+    $PY_ARGS
