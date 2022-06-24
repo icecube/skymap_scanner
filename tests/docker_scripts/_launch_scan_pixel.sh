9e7eaac85db9f01d8853897399e3793bcf66683b
Ric Evans <emejqz@gmail.com>
2022-06-24 16:40:57 -0500

    CI: fix pkl scan test

--- tests/docker_scripts/_launch_scan_pixel.sh
+++ tests/docker_scripts/_launch_scan_pixel.sh
@@ -2,9 +2,11 @@
 
 set -x
 
+# NOTE: don't change mount locations, these are embedded in the 'in.pkl' file
+
 docker run --network="host" --rm -i \
     --shm-size=6gb \
-    --mount type=bind,source=$SKYSCAN_GCD_DIR,target=/local/gcd \
+    --mount type=bind,source=$SKYSCAN_GCD_DIR,target=/local/gcd-dir \
     --mount type=bind,source="$(pwd)"/$SKYSCAN_CLIENT_SCANNER_FILES_DIR,target=/local/$SKYSCAN_CLIENT_SCANNER_FILES_DIR \
     --env PY_COLORS=1 \
     icecube/skymap_scanner \
