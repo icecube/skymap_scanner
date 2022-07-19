23dd77e0450d472c6b4f0db4b27972cd0b33f226
Ric Evans <emejqz@gmail.com>
2022-07-19 11:45:18 -0500

    use `rtol` w/ `{"llh": 1e-5, "E_in": 1e-4, "E_tot": 1e-2}`

--- skymap_scanner/server/scan_result.py
+++ skymap_scanner/server/scan_result.py
@@ -37,6 +37,8 @@ class ScanResult:
     TODO: implement FITS output.
     """
 
+    require_equal = ["index"]
+    require_close = {"llh": 1e-5, "E_in": 1e-4, "E_tot": 1e-2}
     pixel_type = np.dtype(
         [("index", int), ("llh", float), ("E_in", float), ("E_tot", float)]
     )
@@ -54,17 +56,13 @@ class ScanResult:
         # numpy.array_equal() supports `equal_nan` option only from version 1.19
         return all(
             np.array_equal(self.result[nside], other.result[nside])
-            for nside in (self.result.keys() & other.result.keys())
+            for nside in (self.result.keys() & other.result.keys())  # think different
         )
 
     def is_close(self, other, equal_nan=True) -> bool:
         """
         Checks if two results are close by requiring strict equality on pixel indices and close condition on numeric results.
         """
-
-        require_equal = ["index"]
-        require_close = ["llh", "E_in", "E_tot"]
-
         close = dict()  # one bool for each nside value
 
         # first check which nsides were used
@@ -87,13 +85,20 @@ class ScanResult:
                 continue  # we can't go on
 
             nside_equal = {
-                key: np.array_equal(sre[key], ore[key]) for key in require_equal
+                key: np.array_equal(sre[key], ore[key]) for key in self.require_equal
             }
             nside_close = {
                 # np.allclose() expects equal shapes so we need to check for that first
-                key: (sre[key].shape == ore[key].shape)
-                and np.allclose(sre[key], ore[key], equal_nan=equal_nan)
-                for key in require_close
+                key: (
+                    sre[key].shape == ore[key].shape
+                    and np.allclose(
+                        sre[key],
+                        ore[key],
+                        equal_nan=equal_nan,
+                        rtol=rtol,
+                    )
+                )
+                for key, rtol in self.require_close.items()
             }
             close[nside] = all(nside_equal.values()) and all(nside_close.values())
 
