1ad0219d4d25a350015a20e4d5b57090d0dc4faa
Ric Evans <emejqz@gmail.com>
2022-10-06 15:09:07 -0500

    fix json write

--- skymap_scanner/client/client.py
+++ skymap_scanner/client/client.py
@@ -119,7 +119,7 @@ def main() -> None:
     # read startup.json
     with open(args.startup_json_dir / "startup.json", "rb") as f:
         startup_json_dict = json.load(f)
-    with open("GCDQp_packet.json", "wb") as f:
+    with open("GCDQp_packet.json", "w") as f:
         json.dump(startup_json_dict["GCDQp_packet"], f)
 
     cmd = (
