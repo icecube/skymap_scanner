341205db2577da03cdc644fb276a0a563ce0eedd
Ric Evans <emejqz@gmail.com>
2022-10-12 16:45:17 -0500

    undo all the `(temp)` commit (with fixed data)

--- tests/data/reco_pixel_pkls/transform.py
+++
@@ -1,8 +0,0 @@
-import pickle
-import sys
-
-with open(sys.argv[1], "rb") as f:
-    pframe = pickle.load(f)["pframe"]
-
-with open("new.pkl", "wb") as f:
-    pickle.dump({"pframe": pframe, "reco_algo": "millipede"}, f)
