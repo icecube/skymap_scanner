e3b1802e979830fa576c29bef1c84f3343f3e0fa
Ric Evans <emejqz@gmail.com>
2022-10-12 17:42:53 -0500

    return of the (temp) pt-3

--- tests/data/reco_pixel_pkls/transform.py
+++ tests/data/reco_pixel_pkls/transform.py
@@ -1,8 +1,10 @@
 import pickle
 import sys
 
+import Path
+
 with open(sys.argv[1], "rb") as f:
     pframe = pickle.load(f)["pframe"]
 
-with open("new.pkl", "wb") as f:
+with open(Path(sys.argv[1]).parent / "new.pkl", "wb") as f:
     pickle.dump({"pframe": pframe, "reco_algo": "millipede"}, f)
