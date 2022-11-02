2fdcd8809c557ce8b7e627121b47e782768f182f
Tianlu Yuan <5412915+tianluyuan@users.noreply.github.com>
2022-11-02 13:43:57 -0500

    working plotter in ScanResult

--- skymap_scanner/utils/scan_result.py
+++ skymap_scanner/utils/scan_result.py
@@ -5,6 +5,7 @@ import json
 import logging
 from pathlib import Path
 from typing import Dict, List, Optional, Tuple
+import io
 
 import numpy as np
 
@@ -13,6 +14,22 @@ from .pixelreco import NSidesDict, PixelReco
 
 class InvalidPixelValueError(Exception):
     """Raised when a pixel-value is illegal."""
+
+
+def parse_event_id(event_id_string):
+    parts = event_id_string.split('.')
+    if len(parts) != 3:
+        raise RuntimeError("event ID must have 3 parts separated by '.'")
+
+    if not parts[0].startswith("run"):
+        raise RuntimeError("event ID run part does not start with \"run\"")
+    if not parts[1].startswith("evt"):
+        raise RuntimeError("event ID event part does not start with \"evt\"")
+
+    run = int(parts[0][3:])
+    event = int(parts[1][3:])
+    evt_type = parts[2]
+    return (run, event, evt_type)
 
 
 class ScanResult:
@@ -30,7 +47,7 @@ class ScanResult:
     'recoLossesTotal'. The pixel indices in the input dictionary are in
     general unsorted (python dict are unsorted by design) and are
     incomplete (since fine-grained scans only cover a portion of the
-    HEALPIX area). The class stores the each result in a numpy
+    HEALPIX area). The class stores the each result in a np
     structured array sorted by the pixel index, which is stored in a
     dedicated field.
 
@@ -42,9 +59,10 @@ class ScanResult:
     )
     ATOL = 1.0e-8  # 1.0e-8 is the default used by np.isclose()
 
-    def __init__(self, result: Dict[str, np.ndarray]):
+    def __init__(self, result: Dict[str, np.ndarray], event_id: str=''):
         self.logger = logging.getLogger(__name__)
         self.result = result
+        self.event_id = event_id
 
         # bookkeeping for comparing values
         self.require_close = {  # w/ rtol values
@@ -70,7 +88,7 @@ class ScanResult:
         if self.result.keys() != other.result.keys():
             return False
         # NOTE: will return false if NaN are present
-        # numpy.array_equal() supports `equal_nan` option only from version 1.19
+        # np.array_equal() supports `equal_nan` option only from version 1.19
         return all(
             np.array_equal(self.result[nside], other.result[nside])
             for nside in self.result
@@ -284,7 +302,7 @@ class ScanResult:
         return out
 
     """ 
-    numpy input / output
+    np input / output
     """
 
     def get_nside_string(self):
@@ -302,7 +320,7 @@ class ScanResult:
         result = dict()
         for key in npz.keys():
             result[key] = npz[key]
-        return cls(result=result)
+        return cls(result=result, event_id=Path(filename).stem)
 
     def save(self, event_id, output_path=None) -> Path:
         filename = event_id + "_" + self.get_nside_string() + ".npz"
@@ -310,3 +328,362 @@ class ScanResult:
             filename = output_path / Path(filename)
         np.savez(filename, **self.result)
         return Path(filename)
+
+    def create_plot(self,
+                    dosave=False,
+                    dozoom=False,
+                    log_func=None,
+                    upload_func=None,
+                    final_channels=None):
+        if log_func is None:
+            def log_func(x):
+                print(x)
+
+        if upload_func is None:
+            def upload_func(file_buffer, name, title):
+                pass
+
+        if final_channels is None:
+            final_channels=["#test_messaging"]
+        y_inches = 3.85
+        x_inches = 6
+        dpi = 150 if not dozoom else 1200
+        xsize = x_inches*dpi
+        ysize = xsize//2
+
+        lonra=[-10.,10.]
+        latra=[-10.,10.]
+
+        for k in self.result:
+            if "nside-" not in k:
+                raise RuntimeError("\"nside\" not in result file..")
+
+        run_id, event_id, event_type = parse_event_id(self.event_id)
+
+        # mjd = get_event_mjd(state_dict)
+
+        plot_title = f"Run: {run_id} Event {event_id}: Type: {event_type} MJD: TODO"
+
+        plot_filename = f"{self.event_id}.{'plot_zoomed.' if dozoom else ''}pdf"
+        print(f"saving plot to {plot_filename}")
+
+        nsides = [self.parse_nside(_) for _ in self.result]
+        print(f"available nsides: {nsides}")
+
+        maps = []
+        min_value = np.nan
+        max_value = np.nan
+        minRA=0.
+        minDec=0.
+
+        # theta = np.linspace(np.pi, 0., ysize)
+        dec = np.linspace(-np.pi/2., np.pi/2., ysize)
+
+        # phi   = np.linspace(0., 2.*np.pi, xsize)
+        ra = np.linspace(0., 2.*np.pi, xsize)
+
+        # project the map to a rectangular matrix xsize x ysize
+        RA, DEC = np.meshgrid(ra, dec)
+
+        grid_map = None
+
+        grid_pix = None
+
+        # now plot maps above each other
+        for nside in sorted(nsides):
+            print(("constructing map for nside {0}...".format(nside)))
+            # grid_pix = healpy.ang2pix(nside, THETA, PHI)
+            grid_pix = healpy.ang2pix(nside, DEC + np.pi/2., RA)
+            this_map = np.ones(healpy.nside2npix(nside))*np.inf
+
+            for pixel_data in self.result[f'nside-{nside}']:
+                pixel = pixel_data['index']
+                # show 2*delta_LLH
+                value = 2*pixel_data['llh']
+                if np.isfinite(value):
+                    if np.isnan(min_value) or value < min_value:
+                        minDec, minRA = healpy.pix2ang(nside, pixel)
+                        minDec = minDec - np.pi/2.
+                        min_value = value
+                    if np.isnan(max_value) or value > max_value:
+                        max_value = value
+                this_map[pixel] = value
+
+            if grid_map is None:
+                grid_map = this_map[grid_pix]
+            else:
+                grid_map = np.where( np.isfinite(this_map[grid_pix]), this_map[grid_pix], grid_map)
+
+            del this_map
+
+            print(("done with map for nside {0}...".format(nside)))
+
+        # clean up
+        if grid_pix is not None:
+            del grid_pix
+
+        if grid_map is None:
+            # create an "empty" map if there are no pixels at all
+            grid_pix = healpy.ang2pix(8, DEC + np.pi/2., RA)
+            this_map = np.ones(healpy.nside2npix(8))*np.inf
+            grid_map = this_map[grid_pix]
+            del this_map
+            del grid_pix
+
+        print("min  RA:", minRA *180./np.pi, "deg,", minRA*12./np.pi, "hours")
+        print("min dec:", minDec*180./np.pi, "deg")
+
+        # renormalize
+        if dozoom:
+            grid_map = grid_map - min_value
+            # max_value = max_value - min_value
+            min_value = 0.
+            max_value = 50
+
+        grid_map = np.ma.masked_invalid(grid_map)
+
+        print(f"preparing plot: {plot_filename}...")
+
+        # the color map to use
+        cmap = matplotlib.cm.viridis_r
+        cmap.set_under(alpha=0.) # make underflows transparent
+        cmap.set_bad(alpha=1., color=(1.,0.,0.)) # make NaNs bright red
+
+        # prepare the figure canvas
+        fig = matplotlib.pyplot.figure(figsize=[x_inches,y_inches])
+        if dozoom:
+            ax = fig.add_subplot(111) #,projection='cartesian')
+        else:
+            cmap.set_over(alpha=0.)  # make underflows transparent
+            ax = fig.add_subplot(111,projection='astro mollweide')
+
+        # rasterized makes the map bitmap while the labels remain vectorial
+        # flip longitude to the astro convention
+        image = ax.pcolormesh(ra, dec, grid_map, vmin=min_value, vmax=max_value, rasterized=True, cmap=cmap)
+        # ax.set_xlim(np.pi, -np.pi)
+
+        # Use Green's theorem to compute the area
+        # enclosed by the given contour.
+        def area(vs):
+            a = 0
+            x0,y0 = vs[0]
+            for [x1,y1] in vs[1:]:
+                dx = x1-x0
+                dy = y1-y0
+                a += 0.5*(y0*dx - x0*dy)
+                x0 = x1
+                y0 = y1
+            return a
+
+        contour_levels = (np.array([1.39, 4.61, 11.83, 28.74])+min_value)[:2]
+        contour_labels = [r'50%', r'90%', r'3$\sigma$', r'5$\sigma$'][:2]
+        contour_colors=['k', 'r', 'g', 'b'][:2]
+        leg_element=[]
+        cs_collections = []
+        for level, color in zip(contour_levels, contour_colors):
+            CS = ax.contour(ra, dec, grid_map, levels=[level], colors=[color])
+            cs_collections.append(CS.collections[0])
+            e, _ = CS.legend_elements()
+            leg_element.append(e[0])
+
+        if not dozoom:
+            # graticule
+            ax.set_longitude_grid(30)
+            ax.set_latitude_grid(30)
+            cb = fig.colorbar(image, orientation='horizontal', shrink=.6, pad=0.05, ticks=[min_value, max_value])
+            cb.ax.xaxis.set_label_text(r"$-2 \ln(L)$")
+        else:
+            ax.set_xlabel('right ascension')
+            ax.set_ylabel('declination')
+            cb = fig.colorbar(image, orientation='horizontal', shrink=.6, pad=0.13)
+            cb.ax.xaxis.set_label_text(r"$-2 \Delta \ln (L)$")
+
+            leg_labels = []
+            for i in range(len(contour_labels)):
+                vs = cs_collections[i].get_paths()[0].vertices
+                # Compute area enclosed by vertices.
+                a = area(vs) # will be in square-radians
+                a = a*(180.*180.)/(np.pi*np.pi) # convert to square-degrees
+
+                leg_labels.append(f'{contour_labels[i]} - area: {a:.2f}sqdeg')
+
+            ax.scatter(minRA, minDec, s=20, marker='*', color='black', label=r'scan best-fit', zorder=2)
+            ax.legend(leg_element, leg_labels, loc='lower right', fontsize=8, scatterpoints=1, ncol=2)
+
+            print("Contour Area (90%):", a, "degrees (cartesian)", a*np.cos(minDec)**2, "degrees (scaled)")
+            x_width = 1.6 * np.sqrt(a)
+
+            if np.isnan(x_width):
+                x_width = 1.6*(max(CS.allsegs[i][0][:,0]) - min(CS.allsegs[i][0][:,0]))
+            print(x_width)
+            y_width = 0.5 * x_width
+
+            lower_x = max(minRA  - x_width*np.pi/180., 0.)
+            upper_x = min(minRA  + x_width*np.pi/180., 2 * np.pi)
+            lower_y = max(minDec -y_width*np.pi/180., -np.pi/2.)
+            upper_y = min(minDec + y_width*np.pi/180., np.pi/2.) 
+
+            ax.set_xlim( [lower_x, upper_x][::-1])
+            ax.set_ylim( [lower_y, upper_y])
+
+            ax.xaxis.set_major_formatter(DecFormatter())
+            ax.yaxis.set_major_formatter(DecFormatter())
+
+            factor = 0.25*(np.pi/180.)
+            while (upper_x - lower_x)/factor > 6:
+                 factor *= 2.
+            tick_label_grid = factor
+
+            ax.xaxis.set_major_locator(matplotlib.ticker.MultipleLocator(base=tick_label_grid))
+            ax.yaxis.set_major_locator(matplotlib.ticker.MultipleLocator(base=tick_label_grid))
+
+        # cb.ax.xaxis.labelpad = -8
+        # workaround for issue with viewers, see colorbar docstring
+        cb.solids.set_edgecolor("face")
+
+        ax.set_aspect('equal')
+        ax.tick_params(axis='x', labelsize=10)
+        ax.tick_params(axis='y', labelsize=10)
+
+        # show the grid
+        ax.grid(True, color='k', alpha=0.5)
+
+        from matplotlib import patheffects
+        # Otherwise, add the path effects.
+        effects = [patheffects.withStroke(linewidth=1.1, foreground='w')]
+        for artist in ax.findobj(text.Text):
+            artist.set_path_effects(effects)
+
+        # remove white space around figure
+        spacing = 0.01
+        if not dozoom:
+            fig.subplots_adjust(bottom=spacing, top=1.-spacing, left=spacing+0.04, right=1.-spacing)
+        else:
+            fig.subplots_adjust(bottom=spacing, top=0.92-spacing, left=spacing+0.1, right=1.-spacing)
+
+        # set the title
+        fig.suptitle(plot_title)
+
+        if dosave:
+            print(f"saving: {plot_filename}...")
+
+            fig.savefig(plot_filename, dpi=dpi, transparent=True)
+
+        # use io.BytesIO to save this into a memory buffer
+        imgdata = io.BytesIO()
+        fig.savefig(imgdata, format='png', dpi=dpi, transparent=True)
+        imgdata.seek(0)
+
+        print("done.")
+
+        return imgdata
+
+try:
+    from matplotlib.axes import Axes
+    from matplotlib import text
+    from matplotlib.ticker import Formatter, FixedFormatter, FixedLocator
+    from matplotlib.transforms import Transform, Affine2D
+    from matplotlib.projections import projection_registry
+    from matplotlib.projections.geo import MollweideAxes
+    import matplotlib
+    matplotlib.use('agg')
+    import matplotlib.pyplot
+    import healpy
+
+    ##
+    # Mollweide axes with phi axis flipped and in hours from 24 to 0 instead of
+    #         in degrees from -180 to 180.
+    class RaFormatter(Formatter):
+        def __init__(self):
+            pass
+
+        def __call__(self, x, pos=None):
+            hours = (x / np.pi) * 12.
+            minutes = hours - int(hours)
+            hours = int(hours)
+            minutes = minutes * 60.
+
+            seconds = minutes - int(minutes)
+            minutes = int(minutes)
+            seconds = seconds*60.
+            seconds = int(seconds)
+
+            return r"%0.0f$^\mathrm{h}$%0.0f$^\prime$%0.0f$^{\prime\prime}$" % (hours, minutes, seconds)
+
+    class DecFormatter(Formatter):
+        def __init__(self):
+            pass
+
+        def __call__(self, x, pos=None):
+            degrees = (x / np.pi) * 180.
+            return r"$%0.1f^\circ$" % (degrees)
+            # return r"%0.0f$^\circ$" % (degrees)
+
+    class AstroMollweideAxes(MollweideAxes):
+        name = 'astro mollweide'
+
+        def cla(self):
+            super(AstroMollweideAxes, self).cla()
+            self.set_xlim(0, 2*np.pi)
+
+        def set_xlim(self, *args, **kwargs):
+            Axes.set_xlim(self, 0., 2*np.pi)
+            Axes.set_ylim(self, -np.pi / 2.0, np.pi / 2.0)
+
+        def _get_core_transform(self, resolution):
+            return Affine2D().translate(-np.pi, 0.) + super(AstroMollweideAxes, self)._get_core_transform(resolution)
+
+        class RaFormatter(Formatter):
+            # Copied from matplotlib.geo.GeoAxes.ThetaFormatter and modified
+            def __init__(self, round_to=1.0):
+                self._round_to = round_to
+
+            def __call__(self, x, pos=None):
+                hours = (x / np.pi) * 12.
+                hours = round(15 * hours / self._round_to) * self._round_to / 15
+                return r"%0.0f$^\mathrm{h}$" % hours
+
+        def set_longitude_grid(self, degrees):
+            # Copied from matplotlib.geo.GeoAxes.set_longitude_grid and modified
+            number = (360 // degrees) + 1
+            self.xaxis.set_major_locator(
+                FixedLocator(
+                    np.linspace(0, 2*np.pi, number, True)[1:-1]))
+            self._longitude_degrees = degrees
+            self.xaxis.set_major_formatter(self.RaFormatter(degrees))
+
+        def _set_lim_and_transforms(self):
+            # Copied from matplotlib.geo.GeoAxes._set_lim_and_transforms and modified
+            super(AstroMollweideAxes, self)._set_lim_and_transforms()
+
+            # This is the transform for latitude ticks.
+            yaxis_stretch = Affine2D().scale(np.pi * 2.0, 1.0)
+            yaxis_space = Affine2D().scale(-1.0, 1.1)
+            self._yaxis_transform = \
+                yaxis_stretch + \
+                self.transData
+            yaxis_text_base = \
+                yaxis_stretch + \
+                self.transProjection + \
+                (yaxis_space + \
+                 self.transAffine + \
+                 self.transAxes)
+            self._yaxis_text1_transform = \
+                yaxis_text_base + \
+                Affine2D().translate(-8.0, 0.0)
+            self._yaxis_text2_transform = \
+                yaxis_text_base + \
+                Affine2D().translate(8.0, 0.0)
+
+        def _get_affine_transform(self):
+            transform = self._get_core_transform(1)
+            xscale, _ = transform.transform_point((0, 0))
+            _, yscale = transform.transform_point((0, np.pi / 2.0))
+            return Affine2D() \
+                .scale(0.5 / xscale, 0.5 / yscale) \
+                .translate(0.5, 0.5)
+
+    projection_registry.register(AstroMollweideAxes)
+
+except ModuleNotFoundError:
+    pass
