99cc728af622b00263fe0f2e2bb5c6f03eedc28e
Ric Evans <ric@evans-jacquez.com>
2023-01-26 12:58:29 -0600

    validate real event run range: `1e5 <= x < 1e6`

--- skymap_scanner/utils/event_tools.py
+++ skymap_scanner/utils/event_tools.py
@@ -2,7 +2,8 @@
 
 import dataclasses as dc
 
-MIN_REAL_RUN_ID = 100000
+MIN_REAL_RUN_ID_INC = 1e5
+MAX_REAL_RUN_ID_EXC = 1e6  # 1 million
 
 
 @dc.dataclass
@@ -16,9 +17,12 @@ class EventMetadata:
     is_real_event: bool  # as opposed to simulation
 
     def __post_init__(self) -> None:
-        if self.is_real_event and self.run_id < MIN_REAL_RUN_ID:
+        if self.is_real_event and not (
+            MIN_REAL_RUN_ID_INC <= self.run_id < MAX_REAL_RUN_ID_EXC
+        ):
             raise ValueError(
-                f"Run ID Out of Range: {self.run_id} (valid range is {MIN_REAL_RUN_ID}+)"
+                f"Run ID Out of Range for Real Event: {self.run_id} "
+                f"(valid range is [{MIN_REAL_RUN_ID_INC}, {MAX_REAL_RUN_ID_EXC}))"
             )
 
     def __str__(self) -> str:
