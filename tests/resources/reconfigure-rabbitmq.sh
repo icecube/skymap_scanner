cd72d73d7bbb7bb19d6229d58c9b1a98bff5a266
Ric Evans <ric@evans-jacquez.com>
2023-02-16 16:30:57 -0600

    require passing args to reconfigure script

--- tests/resources/reconfigure-rabbitmq.sh
+++ tests/resources/reconfigure-rabbitmq.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-CUSTOM_CONF="/home/runner/rabbitmq_conf/custom.conf"
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "ERROR: Missing dirpath(s) (usage: reconfigure-rabbitmq.sh SRC_DIRPATH DEST_DIRPATH)"
+    exit 1
+
+CUSTOM_CONF="$1/custom.conf"
 
 sudo bash -c "\n## Custom\n' >> $CUSTOM_CONF"
 sudo bash -c "echo -e 'log.console.level = debug\n' >> $CUSTOM_CONF"
 sudo bash -c "echo -e 'loopback_users = none\n' >> $CUSTOM_CONF"  # allows guest/guest from non-localhost
 
 # refresh rabbitmq config - based on ENTRYPOINT logic
-docker exec rabbitmq bash -c 'cat /bitnami/conf/custom.conf >> /opt/bitnami/rabbitmq/etc/rabbitmq/rabbitmq.conf'
+docker exec rabbitmq bash -c "cat $2/custom.conf >> /opt/bitnami/rabbitmq/etc/rabbitmq/rabbitmq.conf"
 
 # sleep to prevent interleaved output in GHA logs
 docker exec rabbitmq cat /opt/bitnami/rabbitmq/etc/rabbitmq/rabbitmq.conf && sleep 1
