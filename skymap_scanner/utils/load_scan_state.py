91a5a94f7b51fdacbfe3a6c39c85f8a6222cc9af
Massimiliano Lincetto <m.lincetto@gmail.com>
2023-03-03 15:17:41 +0100

    readability

--- skymap_scanner/utils/load_scan_state.py
+++ skymap_scanner/utils/load_scan_state.py
@@ -139,17 +139,17 @@ def load_GCDQp_state(event_metadata: EventMetadata, filestager=None, cache_dir="
             raise RuntimeError(f"Cache state seems to require a baseline GCD file (it contains a cached version), but the cache does not have \"{cfg.SOURCE_BASELINE_GCD_METADATA}\". This is a bug or corrupted data.")
         else:
             if filestager is None:
-                orig_packet = None
+                source_baseline_GCD = None
                 for GCD_base_dir in cfg.GCD_BASE_DIRS:
                     try:
                         read_path = os.path.join(GCD_base_dir, source_baseline_GCD_metadata)
-                        LOGGER.debug("load_GCDQp_state => reading GCD from {0}".format(read_path))
-                        orig_packet = load_framepacket_from_file(read_path)
+                        LOGGER.debug("load_GCDQp_state => reading source baseline GCD from {0}".format(read_path))
+                        source_baseline_GCD = load_framepacket_from_file(read_path)
                     except:
                         LOGGER.debug(" -> failed")
-                        orig_packet = None
-                    if orig_packet is None:
-                        raise RuntimeError("load_GCDQp_state => Could not read the input GCD file \"{0}\" from any pre-configured location".format(source_baseline_GCD_metadata))
+                        source_baseline_GCD = None
+                    if source_baseline_GCD is None:
+                        raise RuntimeError("load_GCDQp_state => Could not read the source GCD file \"{0}\" from any pre-configured location".format(source_baseline_GCD_metadata))
             else:
                 # try to load the base file from the various possible input directories
                 GCD_diff_base_handle = None
@@ -170,15 +170,15 @@ def load_GCDQp_state(event_metadata: EventMetadata, filestager=None, cache_dir="
                 if GCD_diff_base_handle is None:
                     raise RuntimeError("Could not read the input GCD file \"{0}\" from any pre-configured location".format(source_baseline_GCD_metadata))
 
-                orig_packet = load_framepacket_from_file( str(GCD_diff_base_handle) )
+                source_baseline_GCD = load_framepacket_from_file( str(GCD_diff_base_handle) )
             this_packet = load_framepacket_from_file(cached_baseline_GCD)
             
-            orig_packet_hash = hash_frame_packet(orig_packet)
+            orig_packet_hash = hash_frame_packet(source_baseline_GCD)
             this_packet_hash = hash_frame_packet(this_packet)
             if orig_packet_hash != this_packet_hash:
                 raise RuntimeError("cached GCD baseline file is different from the original file")
 
-            del orig_packet
+            del source_baseline_GCD
             del this_packet
             
             LOGGER.debug(" - has a frame diff packet at {0} (using original copy)".format(os.path.join(GCD_base_dir, source_baseline_GCD_metadata)))
