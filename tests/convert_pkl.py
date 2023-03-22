f801925b5c522f9c2bf826ede699ce76f6f49eda
Ric Evans <ric@evans-jacquez.com>
2023-03-22 12:46:14 -0500

    update pkl files

--- tests/convert_pkl.py
+++ tests/convert_pkl.py
@@ -1,4 +1,7 @@
-"""A script to help when a pickle format changes."""
+"""A script to help when a pickle format changes.
+
+The should be used as a one-off script & updated as needed.
+"""
 
 import dataclasses as dc  # noqa: F401
 import pickle
@@ -11,7 +14,7 @@ OUT_PKL_FPATH = sys.argv[1]  # "/local/pkls/1660761104.474899.out.pkl"
 with open(OUT_PKL_FPATH, "rb") as f:
     pixreco = pickle.load(f)
     print(pixreco)
-with open(OUT_PKL_FPATH + "-new.pkl", "wb") as f:
+with open(OUT_PKL_FPATH, "wb") as f:
     pickle.dump(
         {
             "pixreco": pixreco,
