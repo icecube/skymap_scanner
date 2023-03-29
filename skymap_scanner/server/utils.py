1e2bd6a0d511b6b467e94c5a9f64e80de978904a
Ric Evans <ric@evans-jacquez.com>
2023-03-29 14:07:57 -0500

    make `NSideProgression` hashable (for caching) pt-3

--- skymap_scanner/server/utils.py
+++ skymap_scanner/server/utils.py
@@ -124,7 +124,7 @@ class NSideProgression(OrderedDict[int, int]):
         return int_int_list
 
     def __hash__(self) -> int:  # type: ignore[override]
-        return hash(set(self._get_int_int_list()))
+        return hash(str(self._get_int_int_list()))
 
     @cachetools.func.lru_cache()
     def n_recos_by_nside_lowerbound(self, n_posvar: int) -> Dict[int, int]:
