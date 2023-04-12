07a1bb2aa8bfec04f24580cec154836344cd2336
Massimiliano Lincetto <m.lincetto@gmail.com>
2023-04-12 13:39:52 +0200

    move filestager in traysegment

--- skymap_scanner/recos/millipede_original.py
+++ skymap_scanner/recos/millipede_original.py
@@ -50,7 +50,6 @@ class MillipedeOriginal(RecoInterface):
     # This is why we can use cascade tables
     # _splinedir = os.path.expandvars("$I3_DATA/photon-tables/splines")
     cfg.LOCAL_STAGING_DIR.mkdir(exist_ok=True)
-    filestager = dataio.get_stagers(staging_directory=str(cfg.LOCAL_STAGING_DIR))
 
     def makeSurePulsesExist(frame, pulsesName) -> None:
         if pulsesName not in frame:
@@ -151,6 +150,7 @@ class MillipedeOriginal(RecoInterface):
     @icetray.traysegment
     def traysegment(tray, name, logger, seed=None):
         """Perform MillipedeOriginal reco."""
+        filestager = dataio.get_stagers(staging_directory=str(cfg.LOCAL_STAGING_DIR))
         _base = os.path.join(cfg.SPLINE_DATA_SOURCE, "ems_mie_z20_a10.%s.fits")
         # for fname in [_base % "abs", _base % "prob"]:
         #     if not os.path.exists(fname):
