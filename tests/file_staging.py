ac29626dca927d4c56592e3645b5c4cf9eeb2012
Massimiliano Lincetto <m.lincetto@gmail.com>
2023-04-26 16:47:12 +0200

    make test more like a test

--- tests/file_staging.py
+++ tests/file_staging.py
@@ -1,11 +1,22 @@
+from pathlib import Path
+from typing import Dict
+
 from skymap_scanner.utils.data_handling import DataStager
 from skymap_scanner import config as cfg
 
 
+local_file_list = []
+
+# We first get a list of all local files
 for path in cfg.LOCAL_DATA_SOURCES:
     subpath = path / cfg.LOCAL_SPLINE_SUBDIR
-    content = list(subpath.glob("*"))
-    print(f"Local path {subpath} contains the following elements:\n:{content}")
+    directory_content = subpath.glob("*")
+    for path in directory_content:
+        if path.is_file():
+            # skip directories
+            local_file_list.append(path.name)  # store name (basename)
+
+remote_file_list = ["README"]  # files only expected to be available remotely
 
 datastager = DataStager(
     local_paths=cfg.LOCAL_DATA_SOURCES,
@@ -13,10 +24,18 @@ datastager = DataStager(
     remote_path=f"{cfg.REMOTE_DATA_SOURCE}/{cfg.REMOTE_SPLINE_SUBDIR}",
 )
 
-FILE_LIST = ["README", "ems_mie_z20_a10.abs.fits"]
+datastager.stage_files(local_file_list)
+datastager.stage_files(remote_file_list)
 
-datastager.stage_files(FILE_LIST)
+# ensure that filepaths can be retrieved for all local files
+local_filepaths: Dict[str, str] = dict()
+for filename in local_file_list:
+    print(f"Testing local file: {filename}.")
+    local_filepaths[filename] = datastager.get_local_filepath(filename)
+    assert local_filepaths[filename] == datastager.get_filepath(filename)
+    print(f"File available at {local_filepaths[filename]}.")
 
-for filename in FILE_LIST:
+for filename in remote_file_list:
+    print(f"Testing staging of remote file: {filename}")
     filepath: str = datastager.get_filepath(filename)
-    print(f"{filename} is available at {filepath}.")
+    print(f"File available at {filepath}.")
