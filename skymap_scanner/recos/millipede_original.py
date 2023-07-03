714a86c83f1f5e226991980c1a6d48b7d4b9b685
Massimiliano Lincetto <m.lincetto@gmail.com>
2023-07-03 18:24:20 +0200

    configure residual value for millipede original and wilks

--- skymap_scanner/recos/millipede_original.py
+++ skymap_scanner/recos/millipede_original.py
@@ -156,7 +156,8 @@ class MillipedeOriginal(RecoInterface):
         tray.AddModule(late_pulse_cleaning, "LatePulseCleaning",
                        input_pulses_name=cls.pulsesName,
                        output_pulses_name=cls.pulsesName_cleaned,
-                       orig_pulses_name=cls.pulsesName
+                       orig_pulses_name=cls.pulsesName,
+                       residual=1.5e3*I3Units.ns,
                        )
         return ExcludedDOMs + [cls.pulsesName_cleaned+'TimeWindows']
 
--- skymap_scanner/recos/millipede_wilks.py
+++ skymap_scanner/recos/millipede_wilks.py
@@ -180,8 +180,8 @@ class MillipedeWilks(RecoInterface):
         tray.AddModule(late_pulse_cleaning, "LatePulseCleaning",
                        input_pulses_name=cls.pulsesName,
                        output_pulses_name=cls.pulsesName_cleaned,
-                       orig_pulses_name=cls.pulsesName_orig
-                       )
+                       orig_pulses_name=cls.pulsesName_orig,
+                       residual=1.5e3*I3Units.ns)
         return ExcludedDOMs + [cls.pulsesName_cleaned+'TimeWindows']
 
     @icetray.traysegment
