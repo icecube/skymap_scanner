eb11b5036a3894972b0fdfcae22eb4d53de014f5
Ric Evans <ric@evans-jacquez.com>
2023-07-21 15:18:42 -0700

    Revert "add default for `EWMS_PILOT_TASK_TIMEOUT`"
    This reverts commit 8da9600b1f97912931c58fa0b05e70965edca120.

--- skymap_scanner/client/client.py
+++ skymap_scanner/client/client.py
@@ -94,7 +94,6 @@ def main() -> None:
             timeout_outgoing=cfg.ENV.SKYSCAN_MQ_TIMEOUT_FROM_CLIENTS,
             timeout_wait_for_first_message=cfg.ENV.SKYSCAN_MQ_CLIENT_TIMEOUT_WAIT_FOR_FIRST_MESSAGE,
             debug_dir=args.debug_directory,
-            task_timeout=cfg.ENV.EWMS_PILOT_TASK_TIMEOUT,
         )
     )
     LOGGER.info("Done.")
