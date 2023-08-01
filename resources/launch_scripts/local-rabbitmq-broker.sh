4920b9a57794d1687c75b29cd393f49637c13d88
Ric Evans <ric@evans-jacquez.com>
2023-08-01 09:31:13 -0700

    add `local-rabbitmq-broker.sh`

---
+++ resources/launch_scripts/local-rabbitmq-broker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -ex
+
+########################################################################
+#
+# Runs a rabbitmq broker server (in background)
+#
+########################################################################
+
+cd ./local-broker/
+
+if [ ! -f "docker-rabbitmq.sh" ]; then
+    wget raw.githubusercontent.com/Observation-Management-Service/MQClient/master/resources/rabbitmq-custom.conf
+    wget raw.githubusercontent.com/Observation-Management-Service/MQClient/master/resources/docker-rabbitmq.sh
+fi
+
+./docker-rabbitmq.sh local-rabbitmq  # runs docker container in background
