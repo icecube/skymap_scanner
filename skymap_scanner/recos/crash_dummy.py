dab7ff2e0b643ad36749dbc89593ff62c886d0b3
Ric Evans <ric@evans-jacquez.com>
2023-11-14 15:15:23 -0600

    add reco

---
+++ skymap_scanner/recos/crash_dummy.py
@@ -0,0 +1,44 @@
+"""IceTray segment for a dummy reco (will crash w/in a given probability)."""
+
+
+import random
+from typing import Final
+
+from ..config import ENV
+from . import RecoInterface, dummy
+
+
+class CrashDummy(dummy.Dummy):
+    """Logic for a dummy reco."""
+
+    def traysegment(tray, name, logger, **kwargs):
+        """Perform dummy reco."""
+        dummy.Dummy.traysegment(tray, name, logger, **kwargs)
+
+        def crash(frame):
+            rand = random.random()
+            logger.debug(f"crash probability: {ENV.SKYSCAN_CRASH_DUMMY_PROBABILITY=}")
+
+            if rand < ENV.SKYSCAN_CRASH_DUMMY_PROBABILITY:
+                logger.debug(f"crash! {rand=}")
+
+                # now, pick what to fail with
+                match random.choice(["infinite-loop", "error"]):
+                    case "infinite-loop":
+                        while True:  # to infinity!
+                            continue
+                    case "error":
+                        raise KeyError("intentional crash-dummy error")
+                    case other:
+                        raise Exception(
+                            f"unintentional intentional crash-dummy error ({other})"
+                        )
+
+            else:
+                logger.debug(f"no crash: {rand=}")
+
+        tray.AddModule(crash, "crash")
+
+
+# Provide a standard alias for the reconstruction class provided by this module.
+RECO_CLASS: Final[type[RecoInterface]] = CrashDummy
