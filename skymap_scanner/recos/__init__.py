d5706e0caf3426ed068d9727970284af6e8b1c17
G-Sommani <giacomo.sommani@gmail.com>
2024-03-28 15:59:26 +0100

    provide additional methods to RecoAlgo()

--- skymap_scanner/recos/__init__.py
+++ skymap_scanner/recos/__init__.py
@@ -62,6 +62,18 @@ class RecoInterface(ABC):
         """Returns a list of vectors referenced to the origin that will be used to generate the vertex position variation."""
         pass
 
+    @staticmethod
+    @abstractmethod
+    def get_use_online_ra_dec() -> bool:
+        """Returns a boll whether to use online_ra_dec to point the scan."""
+        pass
+        
+    @staticmethod
+    @abstractmethod
+    def get_particle_name_possibilities() -> List[str]:
+        """Returns a list of possible particle names to retrieve online_ra_dec."""
+        pass
+
     @abstractmethod
     def prepare_frames(self, tray, name, **kwargs) -> None:
         pass
