8880b99e23814a17ebeba225ab60bc9b73fe2724
G-Sommani <giacomo.sommani@gmail.com>
2024-04-02 09:23:00 +0200

    Added RECO_CLASS to splinempe_pointed

--- skymap_scanner/recos/splinempe_pointed.py
+++ skymap_scanner/recos/splinempe_pointed.py
@@ -12,3 +12,5 @@ class SplineMPE_pointed(splinempe.SplineMPE):
     def __init__(self):
         super().__init__()
         self.use_online_ra_dec = True
+
+RECO_CLASS: Final[type[RecoInterface]] = SplineMPE_pointed
