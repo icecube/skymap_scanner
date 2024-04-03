fe50fc023f4aa2f2459ab68fe26f79b7f79f5430
G-Sommani <giacomo.sommani@gmail.com>
2024-04-03 10:18:57 +0200

    fix indenting

--- skymap_scanner/recos/splinempe_pointed.py
+++ skymap_scanner/recos/splinempe_pointed.py
@@ -18,16 +18,16 @@ class SplineMPE_pointed(splinempe.SplineMPE):
 def get_pointing_info(
     p_frame: Optional[I3Frame]
 ) -> Tuple[float, Union[Tuple[float, float], None]]:
-ang_dist = 3.5
-particle_name_possibilities = ["OnlineL2_SplineMPE", "l2_online_SplineMPE"]
-for particle_name in particle_name_possibilities:
-    if particle_name in p_frame.keys():
-        online_dir = p_frame[particle_name].dir
-        online_ra_dec = astro.dir_to_equa(
-            online_dir.zenith,
-            online_dir.azimuth,
-            p_frame["I3EventHeader"].start_time.mod_julian_day_double
-        )
-return ang_dist, online_ra_dec
+    ang_dist = 3.5
+    particle_name_possibilities = ["OnlineL2_SplineMPE", "l2_online_SplineMPE"]
+    for particle_name in particle_name_possibilities:
+        if particle_name in p_frame.keys():
+            online_dir = p_frame[particle_name].dir
+            online_ra_dec = astro.dir_to_equa(
+                online_dir.zenith,
+                online_dir.azimuth,
+                p_frame["I3EventHeader"].start_time.mod_julian_day_double
+            )
+    return ang_dist, online_ra_dec
 
 RECO_CLASS: Final[type[RecoInterface]] = SplineMPE_pointed
