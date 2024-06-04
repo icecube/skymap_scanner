icecube/skymap_scanner

commit d3d99f0f4ec487a45406d6583f810ef733559529

[minor] Include DeepCore DOMs (#264)

dependencies-from-Dockerfile.log
@@ -17,7 +17,7 @@ beniget==0.4.1
 Bottleneck==1.3.2
 breathe==4.35.0
 Brotli==1.0.9
-cachetools==5.3.2
+cachetools==5.3.3
 certifi==2020.6.20
 cffi==1.15.0
 chardet==4.0.0
@@ -33,19 +33,19 @@ decorator==4.4.2
 defusedxml==0.7.1
 docutils==0.20.1
 et-xmlfile==1.0.1
-ewms-pilot==0.17.7
+ewms-pilot==0.18.0
 fonttools==4.29.1
 fs==2.4.12
 gast==0.5.2
 healpy==1.15.0
 htchirp==3.0
-htcondor==23.3.0
+htcondor==23.4.0
 html5lib==1.1
 hypothesis==6.36.0
 icecube-skyreader==1.2.8
 idna==3.3
 imagesize==1.4.1
-iminuit==2.24.0
+iminuit==2.25.2
 importlib-metadata==4.6.4
 iniconfig==1.1.1
 ipython==7.31.1
@@ -128,16 +128,16 @@ tables==3.7.0
 toml==0.10.2
 tornado==6.4
 traitlets==5.1.1
-typing_extensions==4.9.0
+typing_extensions==4.10.0
 ufoLib2==0.13.1
 unicodedata2==14.0.0
 UNKNOWN @ file:///root/nuflux-2.0.4
-urllib3==2.1.0
+urllib3==2.2.1
 urwid==2.1.2
 wcwidth==0.2.5
 webencodings==0.5.1
-wipac-dev-tools==1.8.2
-wipac-rest-tools==1.6.0
+wipac-dev-tools==1.9.1
+wipac-rest-tools==1.6.1
 xlwt==1.3.0
 zipp==1.0.0
 ########################################################################
@@ -150,14 +150,14 @@ beniget==0.4.1
 Bottleneck==1.3.2
 breathe==4.35.0
 ├── docutils [required: >=0.12, installed: 0.20.1]
-└── sphinx [required: >=4.0,!=5.0.0, installed: 7.2.6]
+└── Sphinx [required: >=4.0,!=5.0.0, installed: 7.2.6]
     ├── alabaster [required: >=0.7,<0.8, installed: 0.7.13]
     ├── Babel [required: >=2.9, installed: 2.13.1]
     ├── docutils [required: >=0.18.1,<0.21, installed: 0.20.1]
     ├── imagesize [required: >=1.3, installed: 1.4.1]
     ├── Jinja2 [required: >=3.0, installed: 3.0.3]
     ├── packaging [required: >=21.0, installed: 21.3]
-    ├── pygments [required: >=2.14, installed: 2.17.2]
+    ├── Pygments [required: >=2.14, installed: 2.17.2]
     ├── requests [required: >=2.25.0, installed: 2.25.1]
     ├── snowballstemmer [required: >=2.0, installed: 2.2.0]
     ├── sphinxcontrib-applehelp [required: Any, installed: 1.0.7]
@@ -213,7 +213,7 @@ pickleshare==0.7.5
 pika==1.3.2
 Pillow==9.0.1
 pip==22.0.2
-pipdeptree==2.13.1
+pipdeptree==2.16.1
 pluggy==0.13.0
 ply==3.11
 prompt-toolkit==3.0.28
@@ -244,13 +244,13 @@ scipy==1.8.0
 setuptools==59.6.0
 six==1.16.0
 skymap-scanner
-├── ewms-pilot [required: Any, installed: 0.17.7]
+├── ewms-pilot [required: Any, installed: 0.18.0]
 │   ├── htchirp [required: Any, installed: 3.0]
-│   ├── htcondor [required: Any, installed: 23.3.0]
+│   ├── htcondor [required: Any, installed: 23.4.0]
 │   └── oms-mqclient [required: Any, installed: 2.4.10]
-│       └── wipac-dev-tools [required: Any, installed: 1.8.2]
+│       └── wipac-dev-tools [required: Any, installed: 1.9.1]
 │           ├── requests [required: Any, installed: 2.25.1]
-│           └── typing-extensions [required: Any, installed: 4.9.0]
+│           └── typing_extensions [required: Any, installed: 4.10.0]
 ├── healpy [required: Any, installed: 1.15.0]
 ├── icecube-skyreader [required: Any, installed: 1.2.8]
 │   ├── astropy [required: Any, installed: 5.0.2]
@@ -259,33 +259,33 @@ skymap-scanner
 │   ├── meander [required: Any, installed: 0.0.3]
 │   ├── numpy [required: Any, installed: 1.21.5]
 │   ├── pandas [required: Any, installed: 1.3.5]
-│   └── wipac-dev-tools [required: Any, installed: 1.8.2]
+│   └── wipac-dev-tools [required: Any, installed: 1.9.1]
 │       ├── requests [required: Any, installed: 2.25.1]
-│       └── typing-extensions [required: Any, installed: 4.9.0]
-├── iminuit [required: Any, installed: 2.24.0]
+│       └── typing_extensions [required: Any, installed: 4.10.0]
+├── iminuit [required: Any, installed: 2.25.2]
 │   └── numpy [required: >=1.21, installed: 1.21.5]
 ├── numpy [required: Any, installed: 1.21.5]
 ├── oms-mqclient [required: Any, installed: 2.4.10]
-│   └── wipac-dev-tools [required: Any, installed: 1.8.2]
+│   └── wipac-dev-tools [required: Any, installed: 1.9.1]
 │       ├── requests [required: Any, installed: 2.25.1]
-│       └── typing-extensions [required: Any, installed: 4.9.0]
-├── wipac-dev-tools [required: Any, installed: 1.8.2]
+│       └── typing_extensions [required: Any, installed: 4.10.0]
+├── wipac-dev-tools [required: Any, installed: 1.9.1]
 │   ├── requests [required: Any, installed: 2.25.1]
-│   └── typing-extensions [required: Any, installed: 4.9.0]
-└── wipac-rest-tools [required: Any, installed: 1.6.0]
-    ├── cachetools [required: Any, installed: 5.3.2]
+│   └── typing_extensions [required: Any, installed: 4.10.0]
+└── wipac-rest-tools [required: Any, installed: 1.6.1]
+    ├── cachetools [required: Any, installed: 5.3.3]
     ├── PyJWT [required: !=2.6.0, installed: 2.8.0]
     ├── qrcode [required: Any, installed: 7.4.2]
     │   ├── pypng [required: Any, installed: 0.20220715.0]
-    │   └── typing-extensions [required: Any, installed: 4.9.0]
+    │   └── typing_extensions [required: Any, installed: 4.10.0]
     ├── requests [required: Any, installed: 2.25.1]
     ├── requests-futures [required: Any, installed: 1.0.1]
     │   └── requests [required: >=1.2.0, installed: 2.25.1]
     ├── tornado [required: Any, installed: 6.4]
-    ├── urllib3 [required: >=2.0.4, installed: 2.1.0]
-    └── wipac-dev-tools [required: Any, installed: 1.8.2]
+    ├── urllib3 [required: >=2.0.4, installed: 2.2.1]
+    └── wipac-dev-tools [required: Any, installed: 1.9.1]
         ├── requests [required: Any, installed: 2.25.1]
-        └── typing-extensions [required: Any, installed: 4.9.0]
+        └── typing_extensions [required: Any, installed: 4.10.0]
 sortedcontainers==2.1.0
 soupsieve==2.3.1
 sympy==1.9

dependencies-from-Dockerfile_no_cvmfs.log
@@ -17,7 +17,7 @@ beniget==0.4.1
 Bottleneck==1.3.2
 breathe==4.35.0
 Brotli==1.0.9
-cachetools==5.3.2
+cachetools==5.3.3
 certifi==2020.6.20
 cffi==1.15.0
 chardet==4.0.0
@@ -33,19 +33,19 @@ decorator==4.4.2
 defusedxml==0.7.1
 docutils==0.20.1
 et-xmlfile==1.0.1
-ewms-pilot==0.17.7
+ewms-pilot==0.18.0
 fonttools==4.29.1
 fs==2.4.12
 gast==0.5.2
 healpy==1.15.0
 htchirp==3.0
-htcondor==23.3.0
+htcondor==23.4.0
 html5lib==1.1
 hypothesis==6.36.0
 icecube-skyreader==1.2.8
 idna==3.3
 imagesize==1.4.1
-iminuit==2.24.0
+iminuit==2.25.2
 importlib-metadata==4.6.4
 iniconfig==1.1.1
 ipython==7.31.1
@@ -128,16 +128,16 @@ tables==3.7.0
 toml==0.10.2
 tornado==6.4
 traitlets==5.1.1
-typing_extensions==4.9.0
+typing_extensions==4.10.0
 ufoLib2==0.13.1
 unicodedata2==14.0.0
 UNKNOWN @ file:///root/nuflux-2.0.4
-urllib3==2.1.0
+urllib3==2.2.1
 urwid==2.1.2
 wcwidth==0.2.5
 webencodings==0.5.1
-wipac-dev-tools==1.8.2
-wipac-rest-tools==1.6.0
+wipac-dev-tools==1.9.1
+wipac-rest-tools==1.6.1
 xlwt==1.3.0
 zipp==1.0.0
 ########################################################################
@@ -150,14 +150,14 @@ beniget==0.4.1
 Bottleneck==1.3.2
 breathe==4.35.0
 ├── docutils [required: >=0.12, installed: 0.20.1]
-└── sphinx [required: >=4.0,!=5.0.0, installed: 7.2.6]
+└── Sphinx [required: >=4.0,!=5.0.0, installed: 7.2.6]
     ├── alabaster [required: >=0.7,<0.8, installed: 0.7.13]
     ├── Babel [required: >=2.9, installed: 2.13.1]
     ├── docutils [required: >=0.18.1,<0.21, installed: 0.20.1]
     ├── imagesize [required: >=1.3, installed: 1.4.1]
     ├── Jinja2 [required: >=3.0, installed: 3.0.3]
     ├── packaging [required: >=21.0, installed: 21.3]
-    ├── pygments [required: >=2.14, installed: 2.17.2]
+    ├── Pygments [required: >=2.14, installed: 2.17.2]
     ├── requests [required: >=2.25.0, installed: 2.25.1]
     ├── snowballstemmer [required: >=2.0, installed: 2.2.0]
     ├── sphinxcontrib-applehelp [required: Any, installed: 1.0.7]
@@ -213,7 +213,7 @@ pickleshare==0.7.5
 pika==1.3.2
 Pillow==9.0.1
 pip==22.0.2
-pipdeptree==2.13.1
+pipdeptree==2.16.1
 pluggy==0.13.0
 ply==3.11
 prompt-toolkit==3.0.28
@@ -244,13 +244,13 @@ scipy==1.8.0
 setuptools==59.6.0
 six==1.16.0
 skymap-scanner
-├── ewms-pilot [required: Any, installed: 0.17.7]
+├── ewms-pilot [required: Any, installed: 0.18.0]
 │   ├── htchirp [required: Any, installed: 3.0]
-│   ├── htcondor [required: Any, installed: 23.3.0]
+│   ├── htcondor [required: Any, installed: 23.4.0]
 │   └── oms-mqclient [required: Any, installed: 2.4.10]
-│       └── wipac-dev-tools [required: Any, installed: 1.8.2]
+│       └── wipac-dev-tools [required: Any, installed: 1.9.1]
 │           ├── requests [required: Any, installed: 2.25.1]
-│           └── typing-extensions [required: Any, installed: 4.9.0]
+│           └── typing_extensions [required: Any, installed: 4.10.0]
 ├── healpy [required: Any, installed: 1.15.0]
 ├── icecube-skyreader [required: Any, installed: 1.2.8]
 │   ├── astropy [required: Any, installed: 5.0.2]
@@ -259,33 +259,33 @@ skymap-scanner
 │   ├── meander [required: Any, installed: 0.0.3]
 │   ├── numpy [required: Any, installed: 1.21.5]
 │   ├── pandas [required: Any, installed: 1.3.5]
-│   └── wipac-dev-tools [required: Any, installed: 1.8.2]
+│   └── wipac-dev-tools [required: Any, installed: 1.9.1]
 │       ├── requests [required: Any, installed: 2.25.1]
-│       └── typing-extensions [required: Any, installed: 4.9.0]
-├── iminuit [required: Any, installed: 2.24.0]
+│       └── typing_extensions [required: Any, installed: 4.10.0]
+├── iminuit [required: Any, installed: 2.25.2]
 │   └── numpy [required: >=1.21, installed: 1.21.5]
 ├── numpy [required: Any, installed: 1.21.5]
 ├── oms-mqclient [required: Any, installed: 2.4.10]
-│   └── wipac-dev-tools [required: Any, installed: 1.8.2]
+│   └── wipac-dev-tools [required: Any, installed: 1.9.1]
 │       ├── requests [required: Any, installed: 2.25.1]
-│       └── typing-extensions [required: Any, installed: 4.9.0]
-├── wipac-dev-tools [required: Any, installed: 1.8.2]
+│       └── typing_extensions [required: Any, installed: 4.10.0]
+├── wipac-dev-tools [required: Any, installed: 1.9.1]
 │   ├── requests [required: Any, installed: 2.25.1]
-│   └── typing-extensions [required: Any, installed: 4.9.0]
-└── wipac-rest-tools [required: Any, installed: 1.6.0]
-    ├── cachetools [required: Any, installed: 5.3.2]
+│   └── typing_extensions [required: Any, installed: 4.10.0]
+└── wipac-rest-tools [required: Any, installed: 1.6.1]
+    ├── cachetools [required: Any, installed: 5.3.3]
     ├── PyJWT [required: !=2.6.0, installed: 2.8.0]
     ├── qrcode [required: Any, installed: 7.4.2]
     │   ├── pypng [required: Any, installed: 0.20220715.0]
-    │   └── typing-extensions [required: Any, installed: 4.9.0]
+    │   └── typing_extensions [required: Any, installed: 4.10.0]
     ├── requests [required: Any, installed: 2.25.1]
     ├── requests-futures [required: Any, installed: 1.0.1]
     │   └── requests [required: >=1.2.0, installed: 2.25.1]
     ├── tornado [required: Any, installed: 6.4]
-    ├── urllib3 [required: >=2.0.4, installed: 2.1.0]
-    └── wipac-dev-tools [required: Any, installed: 1.8.2]
+    ├── urllib3 [required: >=2.0.4, installed: 2.2.1]
+    └── wipac-dev-tools [required: Any, installed: 1.9.1]
         ├── requests [required: Any, installed: 2.25.1]
-        └── typing-extensions [required: Any, installed: 4.9.0]
+        └── typing_extensions [required: Any, installed: 4.10.0]
 sortedcontainers==2.1.0
 soupsieve==2.3.1
 sympy==1.9

setup.cfg
@@ -3,9 +3,11 @@ python_min = 3.9
 python_max = 3.11
 package_dirs = skymap_scanner
 
-[metadata]  # generated by wipac:cicd_setup_builder: name, version
+[metadata]  # generated by wipac:cicd_setup_builder: name, version, keywords
 version = attr: skymap_scanner.__version__
 name = skymap-scanner
+keywords =
+	IceCube
 
 [semantic_release]  # fully-generated by wipac:cicd_setup_builder
 version_variable = skymap_scanner/__init__.py:__version__

skymap_scanner/recos/millipede_wilks.py
@@ -138,7 +138,7 @@ class MillipedeWilks(RecoInterface):
         exclusionList = \
         tray.AddSegment(millipede.HighEnergyExclusions, 'millipede_DOM_exclusions',
             Pulses = cls.pulsesName,
-            ExcludeDeepCore='DeepCoreDOMs',
+            ExcludeDeepCore=False,
             ExcludeSaturatedDOMs='SaturatedDOMs',
             ExcludeBrightDOMs='BrightDOMs',
             BrightDOMThreshold=2,
@@ -209,13 +209,13 @@ class MillipedeWilks(RecoInterface):
             ShowerRegularization=1e-14,
             ExcludedDOMs=ExcludedDOMs,
             PartialExclusion=True,
-            ReadoutWindow=self.pulsesName_cleaned+'TimeRange',
+            ReadoutWindow=self.pulsesName_cleaned + 'TimeRange',
             Pulses=self.pulsesName_cleaned,
             BinSigma=2,
             MinTimeWidth=25,
             RelUncertainty=1)
 
-        tray.AddService('I3GSLRandomServiceFactory','I3RandomService')
+        tray.AddService('I3GSLRandomServiceFactory', 'I3RandomService')
 
         tray.context['isimplex'] = lilliput.IMinuitMinimizer(
             MaxIterations=2000,

commit 5270bf1dab12cb5135ef7753505d667d522e7171

Patch for an inconsistency introduced in #264 (#267)
* still flag DeepCoreDOMs for millipede since the pulses are masked already. see #264

* <bot> update dependencies*.log files(s)

* add scipy to setup.cfg for tests

* <bot> update setup.cfg

* <bot> update dependencies*.log files(s)

---------

Co-authored-by: wipacdevbot <developers@icecube.wisc.edu>

dependencies-from-Dockerfile.log
@@ -39,13 +39,13 @@ fs==2.4.12
 gast==0.5.2
 healpy==1.15.0
 htchirp==3.0
-htcondor==23.6.1
+htcondor==23.7.2
 html5lib==1.1
 hypothesis==6.36.0
 icecube-skyreader==1.2.9
 idna==3.3
 imagesize==1.4.1
-iminuit==2.25.2
+iminuit==2.26.0
 importlib-metadata==4.6.4
 iniconfig==1.1.1
 ipython==7.31.1
@@ -128,7 +128,7 @@ tables==3.7.0
 toml==0.10.2
 tornado==6.4
 traitlets==5.1.1
-typing_extensions==4.11.0
+typing_extensions==4.12.1
 ufoLib2==0.13.1
 unicodedata2==14.0.0
 UNKNOWN @ file:///root/nuflux-2.0.4
@@ -137,7 +137,7 @@ urwid==2.1.2
 wcwidth==0.2.5
 webencodings==0.5.1
 wipac-dev-tools==1.9.1
-wipac-rest-tools==1.7.3
+wipac-rest-tools==1.7.5
 xlwt==1.3.0
 zipp==1.0.0
 ########################################################################
@@ -212,7 +212,7 @@ pexpect==4.8.0
 pickleshare==0.7.5
 pika==1.3.2
 Pillow==9.0.1
-pipdeptree==2.19.1
+pipdeptree==2.22.0
 ├── packaging [required: >=23.1, installed: 24.0]
 └── pip [required: >=23.1.2, installed: 24.0]
 pluggy==0.13.0
@@ -241,17 +241,16 @@ pythran==0.10.0
 pytz==2022.1
 PyYAML==5.4.1
 pyzmq==22.3.0
-scipy==1.8.0
 setuptools==59.6.0
 six==1.16.0
 skymap-scanner
 ├── ewms-pilot [required: Any, installed: 0.19.0]
 │   ├── htchirp [required: Any, installed: 3.0]
-│   ├── htcondor [required: Any, installed: 23.6.1]
+│   ├── htcondor [required: Any, installed: 23.7.2]
 │   └── oms-mqclient [required: Any, installed: 2.4.10]
 │       └── wipac-dev-tools [required: Any, installed: 1.9.1]
 │           ├── requests [required: Any, installed: 2.25.1]
-│           └── typing_extensions [required: Any, installed: 4.11.0]
+│           └── typing_extensions [required: Any, installed: 4.12.1]
 ├── healpy [required: Any, installed: 1.15.0]
 ├── icecube-skyreader [required: Any, installed: 1.2.9]
 │   ├── astropy [required: Any, installed: 5.0.2]
@@ -262,23 +261,24 @@ skymap-scanner
 │   ├── pandas [required: Any, installed: 1.3.5]
 │   └── wipac-dev-tools [required: Any, installed: 1.9.1]
 │       ├── requests [required: Any, installed: 2.25.1]
-│       └── typing_extensions [required: Any, installed: 4.11.0]
-├── iminuit [required: Any, installed: 2.25.2]
+│       └── typing_extensions [required: Any, installed: 4.12.1]
+├── iminuit [required: Any, installed: 2.26.0]
 │   └── numpy [required: >=1.21, installed: 1.21.5]
 ├── numpy [required: Any, installed: 1.21.5]
 ├── oms-mqclient [required: Any, installed: 2.4.10]
 │   └── wipac-dev-tools [required: Any, installed: 1.9.1]
 │       ├── requests [required: Any, installed: 2.25.1]
-│       └── typing_extensions [required: Any, installed: 4.11.0]
+│       └── typing_extensions [required: Any, installed: 4.12.1]
+├── scipy [required: Any, installed: 1.8.0]
 ├── wipac-dev-tools [required: Any, installed: 1.9.1]
 │   ├── requests [required: Any, installed: 2.25.1]
-│   └── typing_extensions [required: Any, installed: 4.11.0]
-└── wipac-rest-tools [required: Any, installed: 1.7.3]
+│   └── typing_extensions [required: Any, installed: 4.12.1]
+└── wipac-rest-tools [required: Any, installed: 1.7.5]
     ├── cachetools [required: Any, installed: 5.3.3]
     ├── PyJWT [required: !=2.6.0, installed: 2.8.0]
     ├── qrcode [required: Any, installed: 7.4.2]
     │   ├── pypng [required: Any, installed: 0.20220715.0]
-    │   └── typing_extensions [required: Any, installed: 4.11.0]
+    │   └── typing_extensions [required: Any, installed: 4.12.1]
     ├── requests [required: Any, installed: 2.25.1]
     ├── requests-futures [required: Any, installed: 1.0.1]
     │   └── requests [required: >=1.2.0, installed: 2.25.1]
@@ -286,7 +286,7 @@ skymap-scanner
     ├── urllib3 [required: >=2.0.4, installed: 2.2.1]
     └── wipac-dev-tools [required: Any, installed: 1.9.1]
         ├── requests [required: Any, installed: 2.25.1]
-        └── typing_extensions [required: Any, installed: 4.11.0]
+        └── typing_extensions [required: Any, installed: 4.12.1]
 sortedcontainers==2.1.0
 soupsieve==2.3.1
 sympy==1.9

dependencies-from-Dockerfile_no_cvmfs.log
@@ -39,13 +39,13 @@ fs==2.4.12
 gast==0.5.2
 healpy==1.15.0
 htchirp==3.0
-htcondor==23.6.1
+htcondor==23.7.2
 html5lib==1.1
 hypothesis==6.36.0
 icecube-skyreader==1.2.9
 idna==3.3
 imagesize==1.4.1
-iminuit==2.25.2
+iminuit==2.26.0
 importlib-metadata==4.6.4
 iniconfig==1.1.1
 ipython==7.31.1
@@ -128,7 +128,7 @@ tables==3.7.0
 toml==0.10.2
 tornado==6.4
 traitlets==5.1.1
-typing_extensions==4.11.0
+typing_extensions==4.12.1
 ufoLib2==0.13.1
 unicodedata2==14.0.0
 UNKNOWN @ file:///root/nuflux-2.0.4
@@ -137,7 +137,7 @@ urwid==2.1.2
 wcwidth==0.2.5
 webencodings==0.5.1
 wipac-dev-tools==1.9.1
-wipac-rest-tools==1.7.3
+wipac-rest-tools==1.7.5
 xlwt==1.3.0
 zipp==1.0.0
 ########################################################################
@@ -212,7 +212,7 @@ pexpect==4.8.0
 pickleshare==0.7.5
 pika==1.3.2
 Pillow==9.0.1
-pipdeptree==2.19.1
+pipdeptree==2.22.0
 ├── packaging [required: >=23.1, installed: 24.0]
 └── pip [required: >=23.1.2, installed: 24.0]
 pluggy==0.13.0
@@ -241,17 +241,16 @@ pythran==0.10.0
 pytz==2022.1
 PyYAML==5.4.1
 pyzmq==22.3.0
-scipy==1.8.0
 setuptools==59.6.0
 six==1.16.0
 skymap-scanner
 ├── ewms-pilot [required: Any, installed: 0.19.0]
 │   ├── htchirp [required: Any, installed: 3.0]
-│   ├── htcondor [required: Any, installed: 23.6.1]
+│   ├── htcondor [required: Any, installed: 23.7.2]
 │   └── oms-mqclient [required: Any, installed: 2.4.10]
 │       └── wipac-dev-tools [required: Any, installed: 1.9.1]
 │           ├── requests [required: Any, installed: 2.25.1]
-│           └── typing_extensions [required: Any, installed: 4.11.0]
+│           └── typing_extensions [required: Any, installed: 4.12.1]
 ├── healpy [required: Any, installed: 1.15.0]
 ├── icecube-skyreader [required: Any, installed: 1.2.9]
 │   ├── astropy [required: Any, installed: 5.0.2]
@@ -262,23 +261,24 @@ skymap-scanner
 │   ├── pandas [required: Any, installed: 1.3.5]
 │   └── wipac-dev-tools [required: Any, installed: 1.9.1]
 │       ├── requests [required: Any, installed: 2.25.1]
-│       └── typing_extensions [required: Any, installed: 4.11.0]
-├── iminuit [required: Any, installed: 2.25.2]
+│       └── typing_extensions [required: Any, installed: 4.12.1]
+├── iminuit [required: Any, installed: 2.26.0]
 │   └── numpy [required: >=1.21, installed: 1.21.5]
 ├── numpy [required: Any, installed: 1.21.5]
 ├── oms-mqclient [required: Any, installed: 2.4.10]
 │   └── wipac-dev-tools [required: Any, installed: 1.9.1]
 │       ├── requests [required: Any, installed: 2.25.1]
-│       └── typing_extensions [required: Any, installed: 4.11.0]
+│       └── typing_extensions [required: Any, installed: 4.12.1]
+├── scipy [required: Any, installed: 1.8.0]
 ├── wipac-dev-tools [required: Any, installed: 1.9.1]
 │   ├── requests [required: Any, installed: 2.25.1]
-│   └── typing_extensions [required: Any, installed: 4.11.0]
-└── wipac-rest-tools [required: Any, installed: 1.7.3]
+│   └── typing_extensions [required: Any, installed: 4.12.1]
+└── wipac-rest-tools [required: Any, installed: 1.7.5]
     ├── cachetools [required: Any, installed: 5.3.3]
     ├── PyJWT [required: !=2.6.0, installed: 2.8.0]
     ├── qrcode [required: Any, installed: 7.4.2]
     │   ├── pypng [required: Any, installed: 0.20220715.0]
-    │   └── typing_extensions [required: Any, installed: 4.11.0]
+    │   └── typing_extensions [required: Any, installed: 4.12.1]
     ├── requests [required: Any, installed: 2.25.1]
     ├── requests-futures [required: Any, installed: 1.0.1]
     │   └── requests [required: >=1.2.0, installed: 2.25.1]
@@ -286,7 +286,7 @@ skymap-scanner
     ├── urllib3 [required: >=2.0.4, installed: 2.2.1]
     └── wipac-dev-tools [required: Any, installed: 1.9.1]
         ├── requests [required: Any, installed: 2.25.1]
-        └── typing_extensions [required: Any, installed: 4.11.0]
+        └── typing_extensions [required: Any, installed: 4.12.1]
 sortedcontainers==2.1.0
 soupsieve==2.3.1
 sympy==1.9

setup.cfg
@@ -27,6 +27,7 @@ install_requires =
 	iminuit
 	numpy
 	oms-mqclient
+	scipy
 	wipac-dev-tools
 	wipac-rest-tools
 python_requires = >=3.9, <3.12

skymap_scanner/recos/millipede_wilks.py
@@ -138,7 +138,7 @@ class MillipedeWilks(RecoInterface):
         exclusionList = \
         tray.AddSegment(millipede.HighEnergyExclusions, 'millipede_DOM_exclusions',
             Pulses = cls.pulsesName,
-            ExcludeDeepCore=False,
+            ExcludeDeepCore='DeepCoreDOMs',
             ExcludeSaturatedDOMs='SaturatedDOMs',
             ExcludeBrightDOMs='BrightDOMs',
             BrightDOMThreshold=2,