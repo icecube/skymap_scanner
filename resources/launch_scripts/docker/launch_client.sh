5215a91e4500bbe7ae7a72e0e1888fb2731a85a9
ric-evans <emejqz@gmail.com>
2024-09-25 16:38:54 -0500

    fix startup.json handling

--- resources/launch_scripts/docker/launch_client.sh
+++ resources/launch_scripts/docker/launch_client.sh
@@ -66,8 +66,14 @@ tmp_rootdir="$(pwd)/pilot-$(uuidgen)"
 mkdir $tmp_rootdir
 cd $tmp_rootdir
 
+# TODO - remove, this should go into the pilot
+datahub="$EWMS_PILOT_DATA_DIR_PARENT_PATH_ON_HOST/ewms-pilot-data/data-hub"
+mkdir -p "$datahub"
+
+# task image, args, env
 export EWMS_PILOT_TASK_IMAGE=$DOCKER_IMAGE_TAG
-export EWMS_PILOT_TASK_ARGS='python -m skymap_scanner.client.reco_icetray --infile {{INFILE}} --outfile {{OUTFILE}} --client-startup-json $EWMS_TASK_DATA_HUB_DIR/startup.json'
+# TODO - add {{DATAHUB}} to pilot, we should (and won't always) know where the datahub is
+export EWMS_PILOT_TASK_ARGS="python -m skymap_scanner.client.reco_icetray --infile {{INFILE}} --outfile {{OUTFILE}} --client-startup-json $datahub/startup.json"
 json_var=$(env | grep '^SKYSCAN_' | awk -F= '{printf "\"%s\":\"%s\",", $1, $2}' | sed 's/,$//') # must remove last comma
 json_var="{$json_var}"
 export EWMS_PILOT_TASK_ENV_JSON="$json_var"
@@ -89,9 +95,6 @@ export EWMS_PILOT_QUEUE_OUTGOING="$SKYSCAN_MQ_FROMCLIENT"
 export EWMS_PILOT_QUEUE_OUTGOING_AUTH_TOKEN="$SKYSCAN_MQ_FROMCLIENT_AUTH_TOKEN"
 export EWMS_PILOT_QUEUE_OUTGOING_BROKER_TYPE="$SKYSCAN_MQ_FROMCLIENT_BROKER_TYPE"
 export EWMS_PILOT_QUEUE_OUTGOING_BROKER_ADDRESS="$SKYSCAN_MQ_FROMCLIENT_BROKER_ADDRESS"
-
-# TODO - remove, this should go into the pilot
-mkdir -p "$EWMS_PILOT_DATA_DIR_PARENT_PATH_ON_HOST/ewms-pilot-data/data-hub"
 
 ENV="$(dirname $tmp_rootdir)/pyenv-$(basename $tmp_rootdir)"
 pip install virtualenv
