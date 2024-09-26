aad4157ba28c656346af6343ff3ed6f76396e0c5
ric-evans <emejqz@gmail.com>
2024-09-26 11:52:01 -0500

    fix startup.json handling - 3

--- resources/launch_scripts/docker/launch_client.sh
+++ resources/launch_scripts/docker/launch_client.sh
@@ -71,10 +71,11 @@ export EWMS_PILOT_DATA_DIR_PARENT_PATH_ON_HOST="$tmp_rootdir"
 # TODO - remove, this should go into the pilot
 datahub="$EWMS_PILOT_DATA_DIR_PARENT_PATH_ON_HOST/ewms-pilot-data/data-hub"
 mkdir -p "$datahub"
+datahub_in_task="/ewms-pilot-data/data-hub"
 
 # task image, args, env
 export EWMS_PILOT_TASK_IMAGE=$DOCKER_IMAGE_TAG
-export EWMS_PILOT_TASK_ARGS="python -m skymap_scanner.client.reco_icetray --infile {{INFILE}} --outfile {{OUTFILE}} --client-startup-json $datahub/startup.json"
+export EWMS_PILOT_TASK_ARGS="python -m skymap_scanner.client.reco_icetray --infile {{INFILE}} --outfile {{OUTFILE}} --client-startup-json $datahub_in_task/startup.json"
 json_var=$(env | grep '^SKYSCAN_' | awk -F= '{printf "\"%s\":\"%s\",", $1, $2}' | sed 's/,$//') # must remove last comma
 json_var="{$json_var}"
 export EWMS_PILOT_TASK_ENV_JSON="$json_var"
