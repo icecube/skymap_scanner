b1f20241eb5621bcd1e5ead16c4fe30136cc1ffe
ric-evans <emejqz@gmail.com>
2025-04-03 15:24:12 -0500

    log `self.pos_variations`

--- skymap_scanner/server/start_scan.py
+++ skymap_scanner/server/start_scan.py
@@ -171,7 +171,7 @@ class PixelsToReco:
             ang_dist=self.reco.ang_dist,
             coord_ra_dec=self.pointing_ra_dec,
         )
-        LOGGER.info(f"Chose {len(pixels_to_refine)} pixels.")
+        LOGGER.info(f"Chose {len(pixels_to_refine)} pixels ({self.pos_variations=}).")
         #
         pixels_to_refine = set(p for p in pixels_to_refine if not pixel_already_sent(p))
         LOGGER.info(f"Filtered down to {len(pixels_to_refine)} pixels (others already sent).")
